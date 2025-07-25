2ba608969132d494780d02ea239800b2ed79d2d2
biantianyu <919284750@qq.com>
2025-07-25 22:35:09 +0800

    SZHDZ-3329 移除lockversion、tenantid等用户无用隐私字段。

--- test-common-result.java
+++
@@ -1,8 +0,0 @@
-import com.cmsr.onebase.framework.common.pojo.CommonResult;
-
-public class TestCommonResult {
-    public static void main(String[] args) {
-        CommonResult<String> result = CommonResult.success("Test");
-        System.out.println("CommonResult test successful: " + result);
-    }
-}
