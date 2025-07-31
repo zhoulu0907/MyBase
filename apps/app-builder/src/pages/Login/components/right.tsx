0ffe92c0718f2ccb14be889a039ccbb730c0b21a
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-07-31 17:29:00 +0800

    SZHDZ-3218 update

--- apps/app-builder/src/pages/Login/components/right.tsx
+++ apps/app-builder/src/pages/Login/components/right.tsx
@@ -1,4 +1,4 @@
-// import LogoSVG from '@/assets/images/logo.svg';
+import LogoSVG from '@/assets/images/logo.svg';
 import {
     Button,
     Checkbox,
@@ -11,7 +11,6 @@ import {
 } from '@arco-design/web-react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LogoSVG from '../../../assets/images/logo.svg';
 
 /**
  * // @ts-expect-error: no types for sm-crypto
