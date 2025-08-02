695e0055fbc5361357c7fb1566959cee2c90291e
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-08-02 12:45:24 +0800

    SZHDZ-3218 update editor

--- apps/app-builder/src/pages/Editor/components/config/index.tsx
+++ apps/app-builder/src/pages/Editor/components/config/index.tsx
@@ -1,4 +1,7 @@
 import { usePageEditorStore } from '@/hooks/useStore';
+import { Button } from '@arco-design/web-react';
+import { IconLeft, IconRight } from '@arco-design/web-react/icon';
+import { useState } from 'react';
 import MaterialConfiger from './components';
 import styles from './index.module.less';
 
@@ -10,8 +13,24 @@ export default function EditorConfig({ }: EditorConfigProps) {
   const { curComponentID } = usePageEditorStore();
 
 
+  const [showDrawer, setShowDrawer] = useState(true);
+
+
   return (
-    <div className={styles.editorConfig}>
+
+    <div className={styles.editorConfig}
+        style={{
+            width: showDrawer ? "310px" : "0px",
+            transition: "width 0.3s cubic-bezier(0.4, 0, 0.2, 1)",
+        }}
+    >
+        <Button
+            size="mini"
+            className={styles.drawerButton}
+            icon={showDrawer ? <IconRight /> : <IconLeft />}
+            type="default"
+            onClick={() => setShowDrawer(!showDrawer)}
+        />
         <MaterialConfiger cpID={curComponentID}/>
     </div>
   );
