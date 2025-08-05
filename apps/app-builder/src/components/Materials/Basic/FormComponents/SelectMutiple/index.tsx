76b89644310bda7556ef175217d8b566e4ede01e
ext_yangxinyu <15639273835@163.com>
2025-08-05 15:25:38 +0800

    SZHDZ-3324 form components bugfix

--- apps/app-builder/src/components/Materials/Basic/FormComponents/SelectMutiple/index.tsx
+++ apps/app-builder/src/components/Materials/Basic/FormComponents/SelectMutiple/index.tsx
@@ -2,7 +2,7 @@ import {
     STATUS_OPTIONS,
     STATUS_VALUES,
 } from "@/components/Materials/constants";
-import { Form, Select, Tooltip } from "@arco-design/web-react";
+import { Form, Select } from "@arco-design/web-react";
 import { memo } from "react";
 import type { XInputSelectMutipleConfig } from "./schema";
 
@@ -16,42 +16,46 @@ const XSelectMutiple = memo((props: XInputSelectMutipleConfig) => {
         status,
         required,
         layout,
+        labelColSpan = 0,
     } = props;
 
     return (
-        <Tooltip content={tooltip}>
-            <Form.Item
-                label={label}
-                layout={layout}
-                rules={[{ required }]}
-                style={{
-                    opacity: status === STATUS_VALUES[STATUS_OPTIONS.HIDDEN] ? 0.5 : 1,
-                    pointerEvents:
-                        status === STATUS_VALUES[STATUS_OPTIONS.READONLY]
-                            ? "none"
-                            : "unset",
-                    margin: '0px',
-                }}
+        <Form.Item
+            label={label}
+            layout={layout}
+            tooltip={tooltip}
+            labelCol={{
+                span: labelColSpan,
+            }}
+            wrapperCol={{ span: 24 - labelColSpan }}
+            rules={[{ required }]}
+            style={{
+                opacity: status === STATUS_VALUES[STATUS_OPTIONS.HIDDEN] ? 0.5 : 1,
+                pointerEvents:
+                    status === STATUS_VALUES[STATUS_OPTIONS.READONLY]
+                        ? "none"
+                        : "unset",
+                margin: '0px',
+            }}
+        >
+            <Select
+                mode="multiple"
+                placeholder="Select"
+                style={{ width: "100%" }}
+                defaultValue={["Beijing", "Shenzhen"]}
+                allowClear
             >
-                <Select
-                    mode="multiple"
-                    placeholder="Select"
-                    style={{ width: "100%" }}
-                    defaultValue={["Beijing", "Shenzhen"]}
-                    allowClear
-                >
-                    {options.map((option, index) => (
-                        <Option
-                            key={option}
-                            disabled={index === 3}
-                            value={option}
-                        >
-                            {option}
-                        </Option>
-                    ))}
-                </Select>
-            </Form.Item>
-        </Tooltip>
+                {options.map((option, index) => (
+                    <Option
+                        key={option}
+                        disabled={index === 3}
+                        value={option}
+                    >
+                        {option}
+                    </Option>
+                ))}
+            </Select>
+        </Form.Item>
     );
 });
 
--- apps/app-builder/src/components/Materials/Basic/FormComponents/UserSelect/index.tsx
+++ apps/app-builder/src/components/Materials/Basic/FormComponents/UserSelect/index.tsx
@@ -2,7 +2,7 @@ import {
     STATUS_OPTIONS,
     STATUS_VALUES,
 } from "@/components/Materials/constants";
-import { Form, Tooltip, TreeSelect } from "@arco-design/web-react";
+import { Form, TreeSelect } from "@arco-design/web-react";
 import { memo } from "react";
 import type { XInputUserSelectConfig } from "./schema";
 
@@ -42,31 +42,35 @@ const XUserSelect = memo((props: XInputUserSelectConfig) => {
         status,
         required,
         layout,
+        labelColSpan = 0,
     } = props;
 
     return (
-        <Tooltip content={tooltip}>
-            <Form.Item
-                label={label}
-                layout={layout}
-                rules={[{ required }]}
-                style={{
-                    opacity: status === STATUS_VALUES[STATUS_OPTIONS.HIDDEN] ? 0.5 : 1,
-                    pointerEvents:
-                        status === STATUS_VALUES[STATUS_OPTIONS.READONLY]
-                            ? "none"
-                            : "unset",
-                    margin: '0px',
-                }}
-            >
-                <TreeSelect
-                    placeholder="Select"
-                    style={{ width: "100%" }}
-                    allowClear
-                    treeData={treeData}
-                />
-            </Form.Item>
-        </Tooltip>
+        <Form.Item
+            label={label}
+            layout={layout}
+            tooltip={tooltip}
+            labelCol={{
+                span: labelColSpan,
+            }}
+            wrapperCol={{ span: 24 - labelColSpan }}
+            rules={[{ required }]}
+            style={{
+                opacity: status === STATUS_VALUES[STATUS_OPTIONS.HIDDEN] ? 0.5 : 1,
+                pointerEvents:
+                    status === STATUS_VALUES[STATUS_OPTIONS.READONLY]
+                        ? "none"
+                        : "unset",
+                margin: '0px',
+            }}
+        >
+            <TreeSelect
+                placeholder="Select"
+                style={{ width: "100%" }}
+                allowClear
+                treeData={treeData}
+            />
+        </Form.Item>
     );
 });
 
