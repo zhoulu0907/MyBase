a884a9849127c89e2b12cae7428a835ac2b6b2aa
AliceYuling <yuyuling@cmsr.chinamobile.com>
2025-08-13 09:06:57 +0800

    SZHDZ-3384 update user type

--- packages/platform-center/src/types/user.ts
+++ packages/platform-center/src/types/user.ts
@@ -1,3 +1,4 @@
+import { SimpleRoleVO } from './role';
 export interface UserVO {
   id: number;
   username: string;
@@ -14,4 +15,5 @@ export interface UserVO {
   remark: string;
   loginDate: Date;
   createTime: Date;
+  roles?: SimpleRoleVO[];
 }
