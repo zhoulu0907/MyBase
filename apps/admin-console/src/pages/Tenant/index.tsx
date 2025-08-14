a5682835c7051cc14984a08c32430da1203bb384
ext_lishuhao <lishuhao004@chinasoftinc.com>
2025-08-14 17:46:00 +0800

    SZHDZ-3331 修改访问地址

--- apps/admin-console/src/pages/Tenant/index.tsx
+++ apps/admin-console/src/pages/Tenant/index.tsx
@@ -391,9 +391,9 @@ const TenantManagement: React.FC = () => {
           <Space className={styles.urlWrapper}>
             {/* http://s25029301301.dev.internal.virtueit.net:81/v0/obappbuilder/#/XXX */}
             <Tooltip position='tl' content={fullUrl}>
-              <Text className={styles.fullUrl} onClick={() => handleClick(displayUrl)}>
+              <Text className={styles.fullUrl} onClick={() => handleClick(fullUrl)}>
                 {/* http://s25029301301.dev.internal.virtueit.net:81/v0/obappbuilder/#/XXX */}
-                {fullUrl}
+                {displayUrl}
               </Text>
             </Tooltip>
             <IconCopy className={styles.copyIcon} onClick={(e) => {
