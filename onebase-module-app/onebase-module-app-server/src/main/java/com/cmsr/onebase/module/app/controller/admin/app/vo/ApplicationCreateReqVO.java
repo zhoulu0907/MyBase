39acf2c33ce5b80999e3e23f57371484941d7996
huangjie <huangjie@cmsr.chinamobile.com>
2025-08-22 13:20:34 +0800

    feat(应用服务): 在应用创建请求中增加数据源信息支持
    添加数据源信息字段到应用创建请求VO，并修改应用服务实现以支持自定义数据源创建

--- onebase-module-app/onebase-module-app-server/src/main/java/com/cmsr/onebase/module/app/controller/admin/app/vo/ApplicationCreateReqVO.java
+++ onebase-module-app/onebase-module-app-server/src/main/java/com/cmsr/onebase/module/app/controller/admin/app/vo/ApplicationCreateReqVO.java
@@ -1,5 +1,6 @@
 package com.cmsr.onebase.module.app.controller.admin.app.vo;
 
+import com.cmsr.onebase.module.metadata.api.datasource.dto.DatasourceSaveReqDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -42,4 +43,8 @@ public class ApplicationCreateReqVO {
 
     @Schema(description = "标签ID")
     private List<Long> tagIds;
+
+    @Schema(description = "数据源信息")
+    private DatasourceSaveReqDTO datasourceSaveReq;
+
 }
