2e719b4818366b9c8c1199e6ad99a10337f42762
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-08-27 11:31:14 +0800

    SZHDZ-3218 dev

--- apps/app-builder/src/pages/Editor/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-import { EDITOR_TYPES } from "./const";
-
-export function isListEditorPage(): boolean {
-  const path = useLocation().pathname;
-  return path.endsWith(`/${EDITOR_TYPES.LIST_EDITOR}`);
-}
