0b1d73b283cd957f80739b7f884c15b95f288c05
yangxiaoyi <yangxiaoyi@cmsr.chinamobile.com>
2025-09-01 15:14:56 +0800

    SZHDZ-3330 ERchart添加渲染标识避免竟态

--- apps/app-builder/src/pages/CreateApp/pages/DataFactory/pages/Entity/components/ERchart/index.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/DataFactory/pages/Entity/components/ERchart/index.tsx
@@ -45,6 +45,7 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
     // const [editingNode, setEditingNode] = useState<EntityNode | null>(null);
     const graphRef = useRef<Graph | null>(null);
     const containerRef = useRef<HTMLDivElement>(null);
+    const isUnmounting = useRef(false);
     const isGraphInitialized = useRef(false); // 标记是否已初始化
     const [zoom, setZoom] = useState(100);
 
@@ -68,6 +69,7 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
       });
 
       return () => {
+        isUnmounting.current = true; // 标记正在卸载
         // Cleanup registration if needed
         try {
           Graph.unregisterNode('er-entity-node');
@@ -76,11 +78,13 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
           console.log(e);
         }
       };
-    }, []); // 添加 mode 到依赖项
+    }, []);
 
     // 初始化图形
     useEffect(() => {
       console.log('Initializing graph...');
+
+      isUnmounting.current = false;
 
       const initGraph = () => {
         if (!containerRef.current) {
@@ -204,7 +208,7 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
       initGraph();
 
       return () => {
-        if (graphRef?.current) {
+        if (graphRef?.current && !isUnmounting.current) {
           graphRef.current?.dispose();
           graphRef.current = null;
           isGraphInitialized.current = false;
@@ -311,7 +315,6 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
                       strokeWidth: 0
                     }
                   },
-                  // position: 'erPortPosition', // 使用您自定义的布局
                   position: {
                     name: 'absolute'
                     // args: { x: 0, y: 0 },
