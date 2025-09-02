61b7968f0fda702839754f4680231b6f7bb3cc30
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-09-02 14:26:25 +0800

    SZHDZ-3218 dev

--- onebase-module-app/onebase-module-app-server/src/main/java/com/cmsr/onebase/module/app/service/appresource/ComponentSerivceImpl.java
+++ onebase-module-app/onebase-module-app-server/src/main/java/com/cmsr/onebase/module/app/service/appresource/ComponentSerivceImpl.java
@@ -1,10 +1,15 @@
 package com.cmsr.onebase.module.app.service.appresource;
 
+import com.cmsr.onebase.framework.common.util.object.BeanUtils;
+import com.cmsr.onebase.module.app.api.appresource.dto.ComponentDTO;
+import com.cmsr.onebase.module.app.dal.dataobject.appresource.ComponentDO;
 import org.springframework.stereotype.Service;
 
 import com.cmsr.onebase.module.app.dal.database.appresource.AppComponentRepository;
 
 import jakarta.annotation.Resource;
+
+import java.util.List;
 
 @Service
 public class ComponentSerivceImpl implements ComponentSerivce {
@@ -16,6 +21,12 @@ public class ComponentSerivceImpl implements ComponentSerivce {
     public Boolean deleteComponent(Long id) {
         appComponentDataRepository.deleteById(id);
         return true;
+    }
+
+    @Override
+    public List<ComponentDTO> listComponent(Long pageId) {
+        List<ComponentDO> componentDOS= appComponentDataRepository.findByPageId(pageId);
+        return BeanUtils.toBean(componentDOS, ComponentDTO.class);
     }
 
 }
