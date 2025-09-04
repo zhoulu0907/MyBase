1cf58f5eacbd81738ad0fbbacffdcb465cc96ec8
huangjie <huangjie@cmsr.chinamobile.com>
2025-09-04 13:58:10 +0800

    feat(flow): 添加 XXL-JOB 支持
    - 新增 JobClient 接口及其实现类 XllJobClientImpl
    - 添加 TimeFlowProcessJob 类用于执行定时任务
    - 新增 XxlJobConfig 配置类
    - 更新 pom.xml，添加 xxl-job 和 unirest 相关依赖

--- onebase-module-flow/onebase-module-flow-server/src/main/java/com/cmsr/onebase/module/flow/client/JobClient.java
+++ onebase-module-flow/onebase-module-flow-server/src/main/java/com/cmsr/onebase/module/flow/client/JobClient.java
@@ -1,23 +1,21 @@
 package com.cmsr.onebase.module.flow.client;
 
 import com.cmsr.onebase.module.flow.client.dto.JobCreateUpdateReqDTO;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 /**
  * @Author：huangjie
  * @Date：2025/9/3 10:07
  */
 //@Component
-public class JobClient {
+public interface JobClient {
 
-    @Value("${flow.job.token}")
-    private String token;
+    void createJob(JobCreateUpdateReqDTO reqDTO);
 
-    @Value("${flow.job.url}")
-    private String url;
+    void updateJob(JobCreateUpdateReqDTO reqDTO);
 
-    public String createJob(JobCreateUpdateReqDTO reqDTO) {
-        return null;
-    }
+    void startJob(Long processId);
+
+    void stopJob(Long processId);
+
+    void deleteJob(Long processId);
 }
