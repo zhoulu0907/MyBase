d1a6254c786c31ad09d6535c193b48c109366e44
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-09-10 17:36:58 +0800

    SZHDZ-3218 refactor

--- packages/ui-kit/src/components/DataFactory/const.ts
+++ packages/ui-kit/src/components/DataFactory/const.ts
@@ -44,37 +44,6 @@ export const ENTITY_FIELD_TYPE = {
   ID: { VALUE: 'ID', LABEL: 'ID' }
 };
 
-// 实体字段类型
-// export const ENTITY_FIELD_TYPE = {
-//   TEXT: '常规文本',
-//   LONG_TEXT: '长文本内容',
-//   EMAIL: '邮箱地址',
-//   PHONE: '电话号码',
-//   URL: '网址链接',
-//   ADDRESS: '详细地址',
-//   NUMBER: '通用数字',
-//   CURRENCY: '货币金额',
-//   DATE: '日期',
-//   DATETIME: '日期时间',
-//   BOOLEAN: '布尔值',
-//   PICKLIST: '单选列表',
-//   MULTI_SELECT: '多选列表',
-//   AUTO_CODE: '自动编号',
-//   USER: '用户引用',
-//   DEPARTMENT: '部门引用',
-//   DATA_SELECTION: '数据选择',
-//   RELATION: '关联关系',
-//   STRUCTURE: '结构化对象',
-//   ARRAY: '数组列表',
-//   FILE: '文件',
-//   IMAGE: '图片',
-//   GEOGRAPHY: '地理位置',
-//   PASSWORD: '密码',
-//   ENCRYPTED: '加密字段',
-//   AGGREGATE: '聚合统计',
-//   ID: '数据ID'
-// };
-
 // 系统字段对应
 export const SYSTEM_FIELD_MAP = {
   id: '数据ID',
