b3aa465ded2841fc5b99102b0dbd5a51394cc5b3
yangxiaoyi <yangxiaoyi@cmsr.chinamobile.com>
2025-09-11 10:04:45 +0800

    SZHDZ-3337 ER图画布布局+节点定位

--- apps/app-builder/src/pages/CreateApp/pages/DataFactory/pages/Entity/components/ERchart/index.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/DataFactory/pages/Entity/components/ERchart/index.tsx
@@ -1,14 +1,13 @@
 import { Graph } from '@antv/x6';
-import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
-// import { ReactShape } from '@antv/x6-react-shape';
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import { register } from '@antv/x6-react-shape';
 import { Button, InputNumber } from '@arco-design/web-react';
 import DetailDrawer from '../Drawers/DetailDrawer';
-// import EditDrawer from '../Drawer/EditDrawer';
 import { type EntityNode, type EntityERProps } from '../../../../utils/interface';
 import { FIELD_TYPE } from '@onebase/ui-kit';
 import EntityNodeComponent from './ERnode';
 import styles from './index.module.less';
+import { GridNodePositioner } from './utils/nodePositioner';
 
 const LINE_HEAD_HEIGHT = 48;
 const LINE_HEIGHT = 34.8;
@@ -41,22 +40,186 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
   ) => {
     const [selectedNode, setSelectedNode] = useState<EntityNode | null>(null);
     const [drawerVisible, setDrawerVisible] = useState(false);
-    // const [editDrawerVisible, seteditDrawerVisible] = useState(false);
-    // const [editingNode, setEditingNode] = useState<EntityNode | null>(null);
+    const [zoom, setZoom] = useState(90);
+
     const graphRef = useRef<Graph | null>(null);
     const containerRef = useRef<HTMLDivElement>(null);
     const isUnmounting = useRef(false);
-    const isGraphInitialized = useRef(false); // 标记是否已初始化
-    const [zoom, setZoom] = useState(100);
+    const isGraphInitialized = useRef(false);
 
     const getGraphPositon = () => {
-      const node = graphRef.current?.getContentArea()?.center;
-      return node;
+      const contentArea = graphRef.current?.getContentArea();
+      return contentArea
+        ? { x: contentArea.x + contentArea.width / 2, y: contentArea.y + contentArea.height / 2 }
+        : undefined;
     };
 
     useImperativeHandle(ref, () => ({
       getGraphPositon
     }));
+
+    // --- 辅助函数：创建节点 ---
+    const createAndAddNode = useCallback(
+      (positioner: any, nodeDatas: EntityNode[]) => {
+        nodeDatas.forEach((nodeData) => {
+          const portsItems = (nodeData: EntityNode) => {
+            const items: object[] = [];
+            nodeData?.fields?.forEach((field, index) => {
+              const extraTitleHeight =
+                field.isSystemField === FIELD_TYPE.SYSTEM ? LINE_TITLE_HEIGHT : LINE_TITLE_HEIGHT * 2;
+              const accumulatedHeight = index >= 1 ? index * LINE_HEIGHT : 0;
+
+              const leftItem = {
+                id: (field?.fieldId || field.fieldName) + '_target', // 使用字段的唯一 ID 作为 port ID
+                group: 'left', // 指定属于 'left' 组
+                args: {
+                  x: 0,
+                  y: LINE_HEAD_HEIGHT + extraTitleHeight + accumulatedHeight + LINE_HEIGHT / 2 // y = 头部高度 + 累积高度 + 额外标题行高度 + 当前字段高度的一半
+                }
+              };
+              const rightItem = {
+                id: (field?.fieldId || field.fieldName) + '_source', // 使用字段的唯一 ID 作为 port ID
+                group: 'right', // 指定属于 'right' 组
+                args: {
+                  x: NODE_WIDTH,
+                  y: LINE_HEAD_HEIGHT + extraTitleHeight + accumulatedHeight + LINE_HEIGHT / 2 // y = 头部高度 + 累积高度 + 额外标题行高度 + 当前字段高度的一半
+                }, // 传递字段索引，用于布局
+                // 可以自定义 attrs 来覆盖 group 的默认 attrs
+                attrs: {
+                  // circle: {
+                  // fill: field.isSystem ? '#f0f0f0' : '#e6f7ff', // 示例：区分系统和自定义字段
+                  // height: currentFieldHeight,
+                  // width: NODE_WIDTH,
+                  // }
+                }
+              };
+              items.push(leftItem);
+              items.push(rightItem);
+            });
+            return items;
+          };
+
+          positioner.addNode({
+            id: nodeData.entityId,
+            x: nodeData?.positionX,
+            y: nodeData?.positionY,
+            width: NODE_WIDTH,
+            height: NODE_HEIGHT,
+            shape: 'er-entity-node',
+            data: {
+              data: nodeData,
+              // mode,
+              onNodeEdit,
+              onNodeAdd,
+              onNodeDelete,
+              onNodeAddField,
+              onNodeAddRelation,
+              onNodeAddMasterDetail,
+              onFieldClick,
+              onStatusChange
+            },
+            attrs: {
+              body: {
+                fill: '#fff',
+                stroke: '#d9d9d9',
+                strokeWidth: 1,
+                rx: 4,
+                ry: 4
+              }
+            },
+            // --- 动态添加 ports ---
+            ports: {
+              // 连接桩分组
+              groups: {
+                left: {
+                  attrs: {
+                    circle: {
+                      // width: NODE_WIDTH,
+                      // height: LINE_HEIGHT,
+                      // strokeWidth: 1,
+                      // stroke: '#5F95FF',
+                      // fill: '#EFF4FF', // 可以根据需要调整颜色
+                      // magnet: true, // 确保 port 是可以连接的
+                      r: 1,
+                      magnet: true,
+                      stroke: 'transparent',
+                      fill: 'transparent',
+                      strokeWidth: 0
+                    }
+                  },
+                  position: {
+                    name: 'absolute'
+                    // args: { x: 0, y: 0 },
+                  }
+                },
+                right: {
+                  attrs: {
+                    circle: {
+                      r: 1,
+                      magnet: true,
+                      stroke: 'transparent',
+                      fill: 'transparent',
+                      strokeWidth: 0
+                    }
+                  },
+                  position: {
+                    name: 'absolute'
+                  }
+                }
+              },
+              // 连接桩定义
+              items: portsItems(nodeData)
+            }
+          });
+        });
+      },
+      [
+        onNodeEdit,
+        onNodeAdd,
+        onNodeDelete,
+        onNodeAddField,
+        onNodeAddRelation,
+        onNodeAddMasterDetail,
+        onFieldClick,
+        onStatusChange
+      ]
+    );
+
+    // --- 辅助函数：创建边 ---
+    const createAndAddEdge = (graph: Graph, edgeData: any) => {
+      // 使用 any 或定义更具体的类型
+      const edge = graph.createEdge({
+        source: { cell: edgeData.sourceEntityId, port: `${edgeData.sourceFieldId}_source` },
+        target: { cell: edgeData.targetEntityId, port: `${edgeData.targetFieldId}_target` },
+        attrs: {
+          line: {
+            stroke: '#39B85F',
+            strokeWidth: 2,
+            targetMarker: { name: 'block', width: 12, height: 8 }
+          }
+        },
+        connector: { name: 'smooth' },
+        data: edgeData,
+        labels: edgeData.label
+          ? [
+              {
+                position: 0.5,
+                attrs: {
+                  label: {
+                    text: edgeData.label,
+                    fill: '#39B85F',
+                    fontSize: 12,
+                    textAnchor: 'middle',
+                    textVerticalAnchor: 'middle'
+                  }
+                }
+              }
+            ]
+          : []
+      });
+      graph.addEdge(edge);
+      return edge;
+    };
 
     useEffect(() => {
       // 注册 React 形状节点
@@ -208,17 +371,16 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
         //   const center = graphRef.current?.getContentArea()?.center;
         //   console.log('center', center);
         // });
-
-        isGraphInitialized.current = true; // 标记已初始化
       };
 
       initGraph();
+
+      graphRef?.current?.zoomTo(zoom / 100);
 
       return () => {
         if (graphRef?.current && !isUnmounting.current) {
           graphRef.current?.dispose();
           graphRef.current = null;
-          isGraphInitialized.current = false;
         }
       };
     }, []);
@@ -226,219 +388,68 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
     useEffect(() => {
       console.log('data changed.');
 
-      // 只在首次渲染时居中，后续更新保持用户操作的位置
-      if (isGraphInitialized.current && data?.nodes?.length) {
-        console.log('First data load, centering content.');
-        graphRef?.current?.zoomToFit({ maxScale: 1 });
-        // graphRef?.current?.centerContent();
+      if (!graphRef.current || !data) {
+        console.log('Graph not ready or no data, skipping update.');
+        return;
       }
+
+      // 初始化 positioner
+      const positioner = new GridNodePositioner(graphRef.current, {
+        startX: 0,
+        startY: 0,
+        columns: 3,
+        nodeWidth: NODE_WIDTH,
+        nodeHeight: 430,
+        horizontalSpacing: 50,
+        verticalSpacing: 50
+      });
 
       graphRef?.current?.clearCells();
 
-      // 添加节点
-      if (data?.nodes) {
-        data?.nodes?.forEach((nodeData) => {
-          const portsItems = (nodeData: EntityNode) => {
-            const items: object[] = [];
-            nodeData?.fields?.forEach((field, index) => {
-              const extraTitleHeight =
-                field.isSystemField === FIELD_TYPE.SYSTEM ? LINE_TITLE_HEIGHT : LINE_TITLE_HEIGHT * 2;
-              const accumulatedHeight = index >= 1 ? index * LINE_HEIGHT : 0;
+      // --- 添加节点 ---
+      if (data.nodes && data.nodes.length > 0) {
+        createAndAddNode(positioner, data.nodes);
+      }
 
-              const leftItem = {
-                id: (field?.fieldId || field.fieldName) + '_target', // 使用字段的唯一 ID 作为 port ID
-                group: 'left', // 指定属于 'left' 组
-                args: {
-                  x: 0,
-                  y: LINE_HEAD_HEIGHT + extraTitleHeight + accumulatedHeight + LINE_HEIGHT / 2 // y = 头部高度 + 累积高度 + 额外标题行高度 + 当前字段高度的一半
-                }
-              };
-              const rightItem = {
-                id: (field?.fieldId || field.fieldName) + '_source', // 使用字段的唯一 ID 作为 port ID
-                group: 'right', // 指定属于 'right' 组
-                args: {
-                  x: NODE_WIDTH,
-                  y: LINE_HEAD_HEIGHT + extraTitleHeight + accumulatedHeight + LINE_HEIGHT / 2 // y = 头部高度 + 累积高度 + 额外标题行高度 + 当前字段高度的一半
-                }, // 传递字段索引，用于布局
-                // 可以自定义 attrs 来覆盖 group 的默认 attrs
-                attrs: {
-                  // circle: {
-                  // fill: field.isSystem ? '#f0f0f0' : '#e6f7ff', // 示例：区分系统和自定义字段
-                  // height: currentFieldHeight,
-                  // width: NODE_WIDTH,
-                  // }
-                }
-              };
-              items.push(leftItem);
-              items.push(rightItem);
-            });
-            return items;
-          };
-          const node = graphRef.current!.createNode({
-            id: nodeData.entityId,
-            x: nodeData.positionX,
-            y: nodeData.positionY,
-            width: NODE_WIDTH,
-            height: NODE_HEIGHT,
-            shape: 'er-entity-node',
-            data: {
-              data: nodeData,
-              // mode,
-              onNodeEdit,
-              onNodeAdd,
-              onNodeDelete,
-              onNodeAddField,
-              onNodeAddRelation,
-              onNodeAddMasterDetail,
-              onFieldClick,
-              onStatusChange
-            },
-            attrs: {
-              body: {
-                fill: '#fff',
-                stroke: '#d9d9d9',
-                strokeWidth: 1,
-                rx: 4,
-                ry: 4
-              }
-            },
-            // --- 动态添加 ports ---
-            ports: {
-              // 连接桩分组
-              groups: {
-                left: {
-                  attrs: {
-                    circle: {
-                      // width: NODE_WIDTH,
-                      // height: LINE_HEIGHT,
-                      // strokeWidth: 1,
-                      // stroke: '#5F95FF',
-                      // fill: '#EFF4FF', // 可以根据需要调整颜色
-                      // magnet: true, // 确保 port 是可以连接的
-                      r: 1,
-                      magnet: true,
-                      stroke: 'transparent',
-                      fill: 'transparent',
-                      strokeWidth: 0
-                    }
-                  },
-                  position: {
-                    name: 'absolute'
-                    // args: { x: 0, y: 0 },
-                  }
-                },
-                right: {
-                  attrs: {
-                    circle: {
-                      r: 1,
-                      magnet: true,
-                      stroke: 'transparent',
-                      fill: 'transparent',
-                      strokeWidth: 0
-                    }
-                  },
-                  position: {
-                    name: 'absolute'
-                  }
-                }
-              },
-              // 连接桩定义
-              items: portsItems(nodeData)
-              // items: nodeData.fields.map((field, index) => {
-              //   const currentFieldHeight = LINE_HEIGHT;
-              //   const extraTitleHeight = field.isSystem ? LINE_TITLE_HEIGHT : LINE_TITLE_HEIGHT * 2;
-              //   const accumulatedHeight = index >= 1 ? index * LINE_HEIGHT: 0;
+      // --- 添加边 ---
+      if (data.edges && data.edges.length > 0) {
+        data.edges.forEach((edge) => createAndAddEdge(graphRef.current!, edge));
+      }
 
-              //   console.log('accumulatedHeight', index, field.id, accumulatedHeight, LINE_HEAD_HEIGHT + accumulatedHeight + extraTitleHeight + currentFieldHeight / 2)
-              //   return {
-              //     id: field.id, // 使用字段的唯一 ID 作为 port ID
-              //     group: 'left', // 指定属于 'list' 组
-              //     args: {
-              //       x: 0,
-              //       y: LINE_HEAD_HEIGHT + extraTitleHeight + accumulatedHeight , // y = 头部高度 + 累积高度 + 额外标题行高度
-              //      }, // 传递字段索引，用于布局
-              //     // 可以自定义 attrs 来覆盖 group 的默认 attrs
-              //     attrs: {
-              //       // circle: {
-              //         // fill: field.isSystem ? '#f0f0f0' : '#e6f7ff', // 示例：区分系统和自定义字段
-              //         // height: currentFieldHeight,
-              //         // width: NODE_WIDTH,
-              //       // }
-              //     }
-              //   };
-              // }),
-            }
-          });
-          graphRef.current?.addNode(node);
+      // 只在首次加载且有节点时进行居中和适配
+      const isFirstLoadWithData = !isGraphInitialized.current && data?.nodes && data.nodes.length > 0;
+
+      // 首次加载画布内容居中显示
+      if (isFirstLoadWithData) {
+        console.log('First data load, fitting and centering content.');
+        requestAnimationFrame(() => {
+          // 检查内容区域是否有效
+          const contentArea = graphRef.current?.getContentArea();
+          const contentBBox = graphRef.current?.getContentBBox();
+
+          console.log('Content area before centering:', contentArea);
+          console.log('Content bbox before centering:', contentBBox);
+
+          if (contentArea && (contentArea.width > 0 || contentArea.height > 0)) {
+            // 使用 zoomToFit 确保内容完全显示在视口内，并自动居中
+            graphRef?.current?.centerContent();
+
+            // 验证居中结果
+            setTimeout(() => {
+              const afterCenterArea = graphRef.current?.getContentArea();
+              const afterCenterBBox = graphRef.current?.getContentBBox();
+              console.log('Content area after centering:', afterCenterArea);
+              console.log('Content bbox after centering:', afterCenterBBox);
+            }, 100);
+
+            isGraphInitialized.current = true;
+            console.log('Graph centered successfully');
+          } else {
+            console.warn('Content area is empty, cannot center');
+          }
         });
-      }
-
-      // 添加边
-      if (data?.edges) {
-        data?.edges?.forEach((edgeData) => {
-          const edge = graphRef.current!.createEdge({
-            source: { cell: edgeData?.sourceEntityId, port: edgeData?.sourceFieldId + '_source' },
-            target: { cell: edgeData?.targetEntityId, port: edgeData?.targetFieldId + '_target' },
-            attrs: {
-              line: {
-                // 连线样式
-                stroke: '#39B85F',
-                strokeWidth: 2,
-                targetMarker: {
-                  name: 'block',
-                  width: 12,
-                  height: 8
-                }
-              }
-            },
-            connector: { name: 'smooth' }, // 曲线
-            // vertices: [ // 路径
-            //   { x: 200, y: 200 },
-            //   { x: 380, y: 120 },
-            // ],
-            data: edgeData,
-            labels: [
-              {
-                position: 0.5,
-                attrs: {
-                  label: {
-                    text: edgeData.label,
-                    fill: '#39B85F',
-                    fontSize: 12,
-                    textAnchor: 'middle',
-                    textVerticalAnchor: 'middle'
-                  }
-                }
-              }
-            ]
-            // 可以添加其他边的属性，如 router, connector 等来优化连线路径
-            // router: 'manhattan', // 例如使用直角路由
-            // connector: { name: 'rounded', args: { radius: 8 }}, // 例如使用圆角连接
-          });
-          graphRef.current?.addEdge(edge);
-        });
-      }
-
-      if (!isGraphInitialized.current && data?.nodes?.length === 1 && !onlyUpdateNode) {
-        console.log('onlyUpdateNode.');
-        // graphRef?.current?.centerPoint(data?.nodes?.[0]?.x || 0, data?.nodes?.[0]?.y - 200 || 0);
-        graphRef?.current?.centerPoint(data?.nodes?.[0]?.positionX || 0, data?.nodes?.[0]?.positionY - 200 || 0);
-      }
-
-      if (isGraphInitialized.current && data?.nodes?.length) {
-        console.log('isFirstRender.');
-        // graphRef?.current?.zoomToFit({ maxScale: 1 });
-        // graphRef?.current?.centerContent();
-        graphRef?.current?.centerPoint(data?.nodes?.[0]?.positionX || 0, data?.nodes?.[0]?.positionY - 200 || 0);
-        isGraphInitialized.current = false;
       }
     }, [data]);
-
-    // 重置缩放
-    const resetZoom = () => {
-      setZoom(100);
-      graphRef?.current?.zoomTo(1);
-    };
 
     // 改变缩放
     const changeZoom = (value: number) => {
@@ -459,12 +470,12 @@ const ERchart = forwardRef<ERchartRef, EntityERProps>(
             max={150}
             min={60}
             step={5}
-            defaultValue={100}
+            defaultValue={90}
             className={styles['zoom-input']}
             value={zoom}
             onChange={(value) => changeZoom(value)}
           />
-          <Button type="outline" size="mini" onClick={() => resetZoom()}>
+          <Button type="outline" size="mini" onClick={() => changeZoom(90)}>
             重置
           </Button>
         </div>
