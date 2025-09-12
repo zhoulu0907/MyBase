6a09f65aecc1979953a2584d2f063a134f717aec
guanshipeng <2533280561@qq.com>
2025-09-12 14:59:37 +0800

    重构项目，剥离运行态、编辑态

---
+++ onebase-module-infra/onebase-module-infra-build/src/main/java/com/cmsr/onebase/module/infra/service/logger/ApiAccessLogServiceImpl.java
@@ -0,0 +1,60 @@
+package com.cmsr.onebase.module.infra.service.logger;
+
+import com.cmsr.onebase.framework.common.biz.infra.logger.dto.ApiAccessLogCreateReqDTO;
+import com.cmsr.onebase.framework.common.pojo.PageResult;
+import com.cmsr.onebase.framework.common.util.object.BeanUtils;
+import com.cmsr.onebase.framework.common.util.string.StrUtils;
+import com.cmsr.onebase.framework.tenant.core.context.TenantContextHolder;
+import com.cmsr.onebase.framework.tenant.core.util.TenantUtils;
+import com.cmsr.onebase.module.infra.dal.database.ApiAccessLogDataRepository;
+import com.cmsr.onebase.module.infra.dal.dataobject.logger.ApiAccessLogDO;
+import com.cmsr.onebase.module.infra.dal.vo.logger.apiaccesslog.ApiAccessLogPageReqVO;
+import com.cmsr.onebase.module.infra.service.logger.ApiAccessLogService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
+
+import static com.cmsr.onebase.module.infra.dal.dataobject.logger.ApiAccessLogDO.REQUEST_PARAMS_MAX_LENGTH;
+import static com.cmsr.onebase.module.infra.dal.dataobject.logger.ApiAccessLogDO.RESULT_MSG_MAX_LENGTH;
+
+/**
+ * API 访问日志 Service 实现类
+ *
+ */
+@Slf4j
+@Service
+@Validated
+public class ApiAccessLogServiceImpl implements ApiAccessLogService {
+
+    @Resource
+    private ApiAccessLogDataRepository apiAccessLogDataRepository;
+
+    @Override
+    public void createApiAccessLog(ApiAccessLogCreateReqDTO createDTO) {
+        ApiAccessLogDO apiAccessLog = BeanUtils.toBean(createDTO, ApiAccessLogDO.class);
+        apiAccessLog.setRequestParams(StrUtils.maxLength(apiAccessLog.getRequestParams(), REQUEST_PARAMS_MAX_LENGTH));
+        apiAccessLog.setResultMsg(StrUtils.maxLength(apiAccessLog.getResultMsg(), RESULT_MSG_MAX_LENGTH));
+        if (TenantContextHolder.getTenantId() != null) {
+            apiAccessLogDataRepository.insert(apiAccessLog);
+        } else {
+            // 极端情况下，上下文中没有租户时，此时忽略租户上下文，避免插入失败！
+            TenantUtils.executeIgnore(() -> apiAccessLogDataRepository.insert(apiAccessLog));
+        }
+    }
+
+    @Override
+    public PageResult<ApiAccessLogDO> getApiAccessLogPage(ApiAccessLogPageReqVO pageReqVO) {
+        return apiAccessLogDataRepository.findPage(pageReqVO);
+    }
+
+    @Override
+    @SuppressWarnings("DuplicatedCode")
+    public Integer cleanAccessLog(Integer exceedDay, Integer deleteLimit) {
+        LocalDateTime expireDate = LocalDateTime.now().minusDays(exceedDay);
+        return apiAccessLogDataRepository.cleanExpiredLogs(expireDate, deleteLimit);
+    }
+
+}
