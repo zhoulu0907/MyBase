64914db6257b15866aa91d50fc3492b9b66efade
HuangJie <huangjie@cmsr.chinamobile.com>
2025-09-15 17:28:25 +0800

    refactor(server-runtime): 重构服务器运行时模块
    - 移除了 AnyLineDBInfoListener 类，减少了不必要的代码
    - 更新了应用配置，添加了 SnailJob 相关配置
    - 重构了 AuthOperationEnum，增加了更多操作类型和相关数据
    - 更新了 pom.xml，替换了依赖并添加了新的依赖项

--- onebase-module-app/onebase-module-app-core/src/main/java/com/cmsr/onebase/module/app/core/enums/auth/AuthOperationEnum.java
+++ onebase-module-app/onebase-module-app-core/src/main/java/com/cmsr/onebase/module/app/core/enums/auth/AuthOperationEnum.java
@@ -1,7 +1,54 @@
 package com.cmsr.onebase.module.app.core.enums.auth;
 
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
 /**
-* @Author：huangjie
-* @Date：2025/9/15  16:16
-*/public enum AuthOperationEnum {
+ * @Author：huangjie
+ * @Date：2025/9/15 16:16
+ */
+public enum AuthOperationEnum {
+
+    CREATE("create", "创建"),
+
+    EDIT("edit", "编辑"),
+
+    DELETE("delete", "删除"),
+
+    IMPORT("import", "导入"),
+
+    EXPORT("export", "导出"),
+
+    SHARE("share", "所有权限");
+
+    private String operation;
+
+    private String label;
+
+    private Data data;
+
+    AuthOperationEnum(String operation, String label) {
+        this.operation = operation;
+        this.label = label;
+        this.data = new Data(operation, label);
+    }
+
+    public static List<Data> getData() {
+        return List.of(
+                CREATE.data,
+                EDIT.data,
+                DELETE.data,
+                IMPORT.data,
+                EXPORT.data,
+                SHARE.data
+        );
+    }
+
+    @lombok.Data
+    @AllArgsConstructor
+    public static class Data {
+        private String operation;
+        private String label;
+    }
 }
