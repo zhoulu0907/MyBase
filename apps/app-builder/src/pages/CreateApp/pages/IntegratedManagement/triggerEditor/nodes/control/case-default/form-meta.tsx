c82b1ef47c71956f02fa5c71acba00baa72d0afe
chenyongqiang <chenyongqiang@richinfo.cn>
2025-09-22 14:07:39 +0800

    SZHDZ-3218 分支和默认分支节点配置修改

--- apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/nodes/control/case-default/form-meta.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/nodes/control/case-default/form-meta.tsx
@@ -349,10 +349,10 @@ export const renderForm = ({ form }: FormRenderProps<FlowNodeJSON['data']>) => {
 
             <Form.Item field="filterCondition" label="条件" required>
               <ConditionEditor
-                onChange={onConditionChange}
                 data={triggerEditorSignal.nodeData.value[node.id]?.filterCondition || []}
                 fields={conditionFields}
                 entityFieldValidationTypes={validationTypes}
+                onConditionChange={onConditionChange}
               />
             </Form.Item>
           </Form>
