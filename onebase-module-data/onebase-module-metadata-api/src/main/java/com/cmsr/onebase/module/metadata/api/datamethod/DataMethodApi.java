8529aa9b068ecfe7d03d35dd36bf7955248af9bb
biantianyu <919284750@qq.com>
2025-09-23 18:16:57 +0800

    SZHDZ-3329 feat(metadata-api): 新增更新实体字段数据接口
    新增 updateData 接口及对应的 UpdateDataReqDTO 请求参数类，用于支持实体字段数据的更新操作。

--- onebase-module-data/onebase-module-metadata-api/src/main/java/com/cmsr/onebase/module/metadata/api/datamethod/DataMethodApi.java
+++ onebase-module-data/onebase-module-metadata-api/src/main/java/com/cmsr/onebase/module/metadata/api/datamethod/DataMethodApi.java
@@ -8,6 +8,7 @@ import com.cmsr.onebase.module.metadata.api.datamethod.dto.DeleteDataReqDTO;
 import com.cmsr.onebase.module.metadata.api.datamethod.dto.EntityFieldDataReqDTO;
 import com.cmsr.onebase.module.metadata.api.datamethod.dto.EntityFieldDataRespDTO;
 import com.cmsr.onebase.module.metadata.api.datamethod.dto.InsertDataReqDTO;
+import com.cmsr.onebase.module.metadata.api.datamethod.dto.UpdateDataReqDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -40,4 +41,12 @@ public interface DataMethodApi {
      * @return 插入成功的数量
      */
     Integer insertData(@Valid @RequestBody InsertDataReqDTO reqDTO);
+
+    /**
+     * 更新实体字段数据
+     *
+     * @param reqDTO 插入请求参数
+     * @return 插入成功的数量
+     */
+    Integer updateData(@Valid @RequestBody UpdateDataReqDTO reqDTO);
 }
