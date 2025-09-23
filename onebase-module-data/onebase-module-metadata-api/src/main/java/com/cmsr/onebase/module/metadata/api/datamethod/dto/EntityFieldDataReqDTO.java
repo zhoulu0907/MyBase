d79325dd2603c52da01a1fe37a9b2b622c8c9521
biantianyu <919284750@qq.com>
2025-09-23 16:33:04 +0800

    SZHDZ-3329feat(metadata-api): 修改条件规则定义为二维数组结构
    将 conditionDTO 字段从单个 ConditionDTO 对象修改为 List<List<ConditionDTO>> 结构，
    以支持外层数组元素间为 OR 关系、内层数组元素间为 AND 关系的规则定义方式。

--- onebase-module-data/onebase-module-metadata-api/src/main/java/com/cmsr/onebase/module/metadata/api/datamethod/dto/EntityFieldDataReqDTO.java
+++ onebase-module-data/onebase-module-metadata-api/src/main/java/com/cmsr/onebase/module/metadata/api/datamethod/dto/EntityFieldDataReqDTO.java
@@ -13,7 +13,7 @@ public class EntityFieldDataReqDTO {
     /**
      * 规则定义二维数组，外层数组元素间为OR关系，内层数组元素间为AND关系
      */
-    private ConditionDTO conditionDTO;
+    private List<List<ConditionDTO>> conditionDTO;
     /**
      * 排序信息
      */
