388d1935d3b78317b6f22208af084f7be17cc2ec
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-10-09 10:29:16 +0800

    SZHDZ-3218 fix bugs

--- apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/components/if-node-condition-editor/index.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/components/if-node-condition-editor/index.tsx
@@ -392,7 +392,6 @@ const IfNodeConditionEditor: React.FC<ConditionEditorProps> = ({ nodeId, form, l
   const getVariableOptions = useCallback(
     (nodeId: string): TreeSelectDataType[] => {
       const nodes = getPrecedingNodes(nodeId, triggerEditorSignal.nodes.value, ALLOW_NODE_TYPES);
-      console.log('xxxNodes: ', nodes);
 
       const options: TreeSelectDataType[] = [];
 
