538c7c2e14c0ca3d84f71c7ab5f96ab000f5bfbd
gaoguoqing_lctx <zhentao789@126.com>
2025-10-09 14:50:05 +0800

    测试git账号是否可用

--- onebase-server/src/main/java/com/cmsr/onebase/server/OneBaseServerApplication.java
+++ onebase-server/src/main/java/com/cmsr/onebase/server/OneBaseServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 项目的启动类
+ * sj
  */
 @SpringBootApplication(scanBasePackages = {"com.cmsr.onebase", "org.anyline"})
 public class OneBaseServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(OneBaseServerApplication.class, args);
     }
 
