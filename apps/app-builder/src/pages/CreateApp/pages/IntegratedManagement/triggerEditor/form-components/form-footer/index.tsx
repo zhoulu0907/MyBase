19c348a83fddfacc7c31f8d0dea977348a497331
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-09 13:28:54 +0800

    SZHDZ-3218 流程图弹窗添加保存、取消逻辑

--- apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/form-components/form-footer/index.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/form-components/form-footer/index.tsx
@@ -1,19 +1,30 @@
 import { Button } from '@arco-design/web-react';
 import { triggerEditorSignal } from '@/store/singals/trigger_editor';
-import React from 'react';
 import styles from './index.module.less';
 
-export function FormFooter({ nodeInfo }: { nodeInfo?: React.ReactNode }) {
-  const saveNode = () => {};
+export function FormFooter({ nodeInfo }: { nodeInfo: any }) {
+  const { nodeId, nodeData, setNodeId, setNodeData } = triggerEditorSignal;
 
-  debugger;
+  const cancel = () => {
+    // 取消 关闭弹窗
+    setNodeId(undefined);
+  };
+  const saveNode = () => {
+    if (nodeId.value) {
+      const formInfo = nodeInfo?.props?.form?.getFieldsValue();
+      const param = { ...nodeData.value[nodeId.value], ...formInfo };
+      setNodeData(nodeId.value, param);
+    }
+  };
 
-  return <div className={styles.formFooter}>
-    <Button type="outline" style={{marginRight:'20px'}} onClick={saveNode}>
-      取消
-    </Button>
-    <Button type="primary" onClick={saveNode}>
-      保存
-    </Button>
-  </div>
+  return (
+    <div className={styles.formFooter}>
+      <Button type="outline" style={{ marginRight: '20px' }} onClick={cancel}>
+        取消
+      </Button>
+      <Button type="primary" onClick={saveNode}>
+        保存
+      </Button>
+    </div>
+  );
 }
