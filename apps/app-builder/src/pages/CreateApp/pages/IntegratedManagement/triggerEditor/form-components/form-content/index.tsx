f191edb14a04b9724ec0cb08b2279df85173965c
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-09 15:43:08 +0800

    SZHDZ-3218 流程图弹窗添加保存、取消逻辑

--- apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/form-components/form-content/index.tsx
+++ apps/app-builder/src/pages/CreateApp/pages/IntegratedManagement/triggerEditor/form-components/form-content/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormFooter} from '../form-footer';
+import { FormFooter } from '../form-footer';
 import { FlowNodeRegistry } from '@flowgram.ai/fixed-layout-editor';
 import { useIsSidebar, useNodeRenderContext } from '../../hooks';
 import styles from './index.module.less';
@@ -14,10 +14,13 @@ export function FormContent(props: { children?: React.ReactNode }) {
   const registry = node.getNodeRegistry<FlowNodeRegistry>();
 
   return (
-    <div className={styles.formWrapper} style={isSidebar ? {height:'calc(100% - 73px)',position:'relative',marginBottom:'49px'}:{}} >
+    <div
+      className={styles.formWrapper}
+      style={isSidebar ? { height: 'calc(100% - 73px)', position: 'relative', marginBottom: '49px' } : {}}
+    >
       <>
         {isSidebar && <div className={styles.formTitleDescription}>{registry.info?.description}</div>}
-        
+
         <div className={styles.formContent}>{(expanded || isSidebar) && props.children}</div>
         {isSidebar && <FormFooter nodeInfo={props.children}></FormFooter>}
       </>
