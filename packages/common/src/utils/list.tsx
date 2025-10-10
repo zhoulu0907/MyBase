4b04449c0f878cd924d3ea05364f8be145123282
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-10 13:26:19 +0800

    SZHDZ-3218 分页查询方法修改

--- packages/common/src/utils/list.tsx
+++ packages/common/src/utils/list.tsx
@@ -1,11 +1,28 @@
 /**
  * 通用分页列表方法 最后一页没有数据问题
- * 请求方法  getLists
+ * 请求方法  getList
  * 请求参数 req
  * 页码设置方法 setPageNo
- * 页码 pageNo
- * total  list 
+ * 页码? pageNo 默认取req里的pageNo
+ * total  list
  */
-export const getCommonPaginationList = async() => {
-  
+interface PageParam {
+  pageNo?: number;
+  pageSize?: number;
+  [key: string]: any;
+}
+export const getCommonPaginationList = async (
+  getList: (param: PageParam) => any,
+  req: PageParam,
+  setPageNo: (num: number) => void
+) => {
+  const res = await getList(req);
+  if (res && (!res.list || res.list.length === 0) && res.total != 0 && req.pageNo > 1) {
+    const newPageNo = req.pageNo - 1;
+    setPageNo(newPageNo);
+    const newReq = { ...req, pageNo: newPageNo };
+    await getCommonPaginationList(getList, newReq, setPageNo);
+  } else {
+    return res;
+  }
 };
