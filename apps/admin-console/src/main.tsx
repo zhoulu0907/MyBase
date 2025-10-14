7bdb646beab50c5842bbd9fb40e59857e57d7d34
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-14 08:39:18 +0800

    SZHDZ-3218 主题样式调整

--- apps/admin-console/src/main.tsx
+++ apps/admin-console/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './i18n';
 import './index.css';
+import '@arco-design/web-react/dist/css/arco.css';
+import '@arco-themes/react-cyansu-ob03/index.less';
+import '@onebase/ui-kit/src/theme.less';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- apps/runtime/src/main.tsx
+++ apps/runtime/src/main.tsx
@@ -7,6 +7,9 @@ import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.tsx';
 import './i18n';
 import './index.css';
+import '@arco-design/web-react/dist/css/arco.css';
+import '@arco-themes/react-cyansu-ob03/index.less';
+import '@onebase/ui-kit/src/theme.less';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
