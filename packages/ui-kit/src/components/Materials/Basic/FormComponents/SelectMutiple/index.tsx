a93848683f2422c20aabfd54e008a9f209c9fe06
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-15 16:26:01 +0800

    SZHDZ-3218 表单下拉多选数据回显

--- packages/ui-kit/src/components/Materials/Basic/FormComponents/SelectMutiple/index.tsx
+++ packages/ui-kit/src/components/Materials/Basic/FormComponents/SelectMutiple/index.tsx
@@ -1,4 +1,4 @@
-import { Form, Select,Tag } from '@arco-design/web-react';
+import { Form, Select, Tag } from '@arco-design/web-react';
 import { memo, useEffect, useState } from 'react';
 import { STATUS_OPTIONS, STATUS_VALUES } from '../../../constants';
 import { FORM_COMPONENT_TYPES } from '../../../componentTypes';
@@ -47,7 +47,7 @@ const XSelectMutiple = memo((props: XInputSelectMutipleConfig & { runtime?: bool
       >
         {status === STATUS_VALUES[STATUS_OPTIONS.READONLY] || detailMode ? (
           <div>
-            {fieldValue && defaultValue && fieldValue.map((ele:any)=><Tag>
+            {fieldValue && defaultValue && fieldValue.map((ele: any) => <Tag>
               {defaultValue.find((e: any) => e.value === ele).label}
             </Tag>)}
           </div>
