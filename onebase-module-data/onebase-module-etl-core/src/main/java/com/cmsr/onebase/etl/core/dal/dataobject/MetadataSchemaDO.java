9244bb765ecd2f33cababed6db91b1a7a9b80d79
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-10-16 15:26:12 +0800

    初始化并添加数据工厂（ETL）相关包

---
+++ onebase-module-data/onebase-module-etl-core/src/main/java/com/cmsr/onebase/etl/core/dal/dataobject/MetadataSchemaDO.java
@@ -0,0 +1,34 @@
+package com.cmsr.onebase.etl.core.dal.dataobject;
+
+import com.cmsr.onebase.framework.tenant.core.db.TenantBaseDO;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Table(name = "metadata_schema")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetadataSchemaDO extends TenantBaseDO {
+
+    public MetadataSchemaDO setId(Long id) {
+        super.setId(id);
+        return this;
+    }
+
+    private Long datasourceId;
+
+    private String fqnHash;
+
+    private String schemaName;
+
+    private String displayName;
+
+    private String metaInfo;
+
+}
