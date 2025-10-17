73dad627bbbbd017421e516ccf04b219aff143f4
chengyuansen <chengyuansen@cmsr.chinamobile.com>
2025-10-17 17:08:45 +0800

    新增Workflow接口封装

--- onebase-framework/onebase-spring-boot-starter-dolphinscheduler/src/main/java/com/cmsr/onebase/framework/dolphins/dto/workflow/enums/ReleaseStateEnum.java
+++ onebase-framework/onebase-spring-boot-starter-dolphinscheduler/src/main/java/com/cmsr/onebase/framework/dolphins/dto/workflow/enums/ReleaseStateEnum.java
@@ -1,8 +1,5 @@
 package com.cmsr.onebase.framework.dolphins.dto.workflow.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 /**
  * 工作流定义发布状态枚举
  *
@@ -14,31 +11,10 @@ public enum ReleaseStateEnum {
     /**
      * 离线状态
      */
-    OFFLINE("OFFLINE"),
+    OFFLINE,
 
     /**
      * 在线状态
      */
-    ONLINE("ONLINE");
-
-    private final String value;
-
-    ReleaseStateEnum(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
-
-    @JsonCreator
-    public static ReleaseStateEnum fromValue(String value) {
-        for (ReleaseStateEnum e : ReleaseStateEnum.values()) {
-            if (e.value.equals(value)) {
-                return e;
-            }
-        }
-        throw new IllegalArgumentException("Unknown ReleaseStateEnum value: " + value);
-    }
+    ONLINE
 }
---
+++ onebase-framework/onebase-spring-boot-starter-dolphinscheduler/src/main/java/com/cmsr/onebase/framework/dolphins/dto/workflowinstance/request/WorkflowInstanceQueryRequestDTO.java
@@ -0,0 +1,67 @@
+package com.cmsr.onebase.framework.dolphins.dto.workflowinstance.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/**
+ * 工作流实例查询请求 DTO
+ * 
+ * 对应 DolphinScheduler API: GET /v2/workflow-instances
+ *
+ * @author matianyu
+ * @date 2025-10-17
+ */
+@Data
+public class WorkflowInstanceQueryRequestDTO {
+
+    /**
+     * 页码号(必填)
+     */
+    @NotNull(message = "页码号不能为空")
+    @JsonProperty("pageNo")
+    private Integer pageNo;
+
+    /**
+     * 页大小(必填)
+     */
+    @NotNull(message = "页大小不能为空")
+    @JsonProperty("pageSize")
+    private Integer pageSize;
+
+    /**
+     * 项目名称
+     */
+    @JsonProperty("projectName")
+    private String projectName;
+
+    /**
+     * 工作流名称
+     */
+    @JsonProperty("workflowName")
+    private String workflowName;
+
+    /**
+     * 主机地址
+     */
+    @JsonProperty("host")
+    private String host;
+
+    /**
+     * 开始时间
+     */
+    @JsonProperty("startDate")
+    private String startDate;
+
+    /**
+     * 结束时间
+     */
+    @JsonProperty("endDate")
+    private String endDate;
+
+    /**
+     * 状态
+     */
+    @JsonProperty("state")
+    private Integer state;
+}
