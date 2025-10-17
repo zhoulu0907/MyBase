f916ffec91e1ed38388570a2c1caac8289e1ae6e
chengyuansen <chengyuansen@cmsr.chinamobile.com>
2025-10-17 15:05:12 +0800

    新增schedule接口代码

---
+++ onebase-framework/onebase-spring-boot-starter-dolphinscheduler/src/main/java/com/cmsr/onebase/framework/dolphins/dto/schedule/request/ScheduleUpdateRequestDTO.java
@@ -0,0 +1,87 @@
+package com.cmsr.onebase.framework.dolphins.dto.schedule.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * 定时调度更新请求 DTO
+ * 
+ * 对应 DolphinScheduler API: PUT /v2/schedules/{id}
+ *
+ * @author matianyu
+ * @date 2025-10-17
+ */
+@Data
+public class ScheduleUpdateRequestDTO {
+
+    /**
+     * Cron表达式(必填)
+     */
+    @NotBlank(message = "Cron表达式不能为空")
+    @JsonProperty("crontab")
+    private String crontab;
+
+    /**
+     * 开始时间(必填,格式: yyyy-MM-dd HH:mm:ss)
+     */
+    @NotBlank(message = "开始时间不能为空")
+    @JsonProperty("startTime")
+    private String startTime;
+
+    /**
+     * 结束时间(必填,格式: yyyy-MM-dd HH:mm:ss)
+     */
+    @NotBlank(message = "结束时间不能为空")
+    @JsonProperty("endTime")
+    private String endTime;
+
+    /**
+     * 时区ID(必填,例如: Asia/Shanghai)
+     */
+    @NotBlank(message = "时区ID不能为空")
+    @JsonProperty("timezoneId")
+    private String timezoneId;
+
+    /**
+     * 失败策略(CONTINUE/END,默认CONTINUE)
+     */
+    @JsonProperty("failureStrategy")
+    private String failureStrategy;
+
+    /**
+     * 发布状态(ONLINE/OFFLINE,默认OFFLINE)
+     */
+    @JsonProperty("releaseState")
+    private String releaseState;
+
+    /**
+     * 告警类型(NONE/SUCCESS/FAILURE/ALL,默认NONE)
+     */
+    @JsonProperty("warningType")
+    private String warningType;
+
+    /**
+     * 告警组ID(默认0)
+     */
+    @JsonProperty("warningGroupId")
+    private Integer warningGroupId;
+
+    /**
+     * 工作流实例优先级(HIGHEST/HIGH/MEDIUM/LOW/LOWEST,默认MEDIUM)
+     */
+    @JsonProperty("workflowInstancePriority")
+    private String workflowInstancePriority;
+
+    /**
+     * Worker组名称
+     */
+    @JsonProperty("workerGroup")
+    private String workerGroup;
+
+    /**
+     * 环境编码
+     */
+    @JsonProperty("environmentCode")
+    private Long environmentCode;
+}
