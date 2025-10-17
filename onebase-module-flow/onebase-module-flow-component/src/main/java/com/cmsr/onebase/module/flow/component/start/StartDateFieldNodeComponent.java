ac92436d33e245dd753577c38ba5de758795e051
HuangJie <huangjie@cmsr.chinamobile.com>
2025-10-17 16:10:20 +0800

    feat(flow): 调整流程任务消息处理逻辑并优化配置
    - 修改 FlowJobMessage 中的 msgType 字段名为 jobType
    -重构 FlowJobMessageHandler 的 consume 方法，增加异常处理和执行结果记录
    - 添加 executeFlow 方法用于统一处理流程执行逻辑
    - 更新 logback-spring.xml 配置文件中的日志格式与 Appender 设置
    - 移除部分模块中不再使用的安全权限相关代码及依赖- 新增 RuntimeLoginUser 和 RuntimeSecurityContext 类用于运行时安全管理- 删除旧的 ScheduleMainTest 测试类- 调整 pom.xml 依赖结构，移除不必要的安全框架依赖项

--- onebase-module-flow/onebase-module-flow-component/src/main/java/com/cmsr/onebase/module/flow/component/start/StartDateFieldNodeComponent.java
+++ onebase-module-flow/onebase-module-flow-component/src/main/java/com/cmsr/onebase/module/flow/component/start/StartDateFieldNodeComponent.java
@@ -1,9 +1,30 @@
 package com.cmsr.onebase.module.flow.component.start;
 
+import com.cmsr.onebase.framework.tenant.core.util.TenantUtils;
+import com.cmsr.onebase.module.flow.component.data.DataMethodApiHelper;
+import com.cmsr.onebase.module.flow.component.utils.ConditionsProvider;
+import com.cmsr.onebase.module.flow.context.ExecuteContext;
 import com.cmsr.onebase.module.flow.context.VariableContext;
+import com.cmsr.onebase.module.flow.context.condition.Conditions;
+import com.cmsr.onebase.module.flow.context.enums.JdbcTypeEnum;
+import com.cmsr.onebase.module.flow.context.express.OrExpression;
+import com.cmsr.onebase.module.flow.context.graph.InLoopDepth;
+import com.cmsr.onebase.module.flow.context.graph.nodes.StartDateFieldNodeData;
+import com.cmsr.onebase.module.metadata.api.datamethod.DataMethodApi;
+import com.cmsr.onebase.module.metadata.api.datamethod.dto.EntityFieldDataReqDTO;
+import com.cmsr.onebase.module.metadata.api.datamethod.dto.EntityFieldDataRespDTO;
+import com.cmsr.onebase.module.metadata.api.entity.MetadataEntityFieldApi;
+import com.cmsr.onebase.module.metadata.api.entity.dto.EntityFieldJdbcTypeReqDTO;
+import com.cmsr.onebase.module.metadata.api.entity.dto.EntityFieldJdbcTypeRespDTO;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @Author：huangjie
@@ -13,12 +34,56 @@ import lombok.extern.slf4j.Slf4j;
 @LiteflowComponent("startDateField")
 public class StartDateFieldNodeComponent extends NodeComponent {
 
+    @Autowired
+    private DataMethodApi dataMethodApi;
+
+    @Autowired
+    private ConditionsProvider conditionsProvider;
+
+    @Autowired
+    private MetadataEntityFieldApi metadataEntityFieldApi;
 
     @Override
     public void process() throws Exception {
         log.info("StartDateFieldNodeComponent process");
+        ExecuteContext executeContext = this.getContextBean(ExecuteContext.class);
         VariableContext variableContext = this.getContextBean(VariableContext.class);
         variableContext.putInputVariables(this.getTag());
+        StartDateFieldNodeData nodeData = (StartDateFieldNodeData) executeContext.getNodeData(this.getTag());
+        InLoopDepth inLoopDepth = nodeData.getInLoopDepth();
+        JdbcTypeEnum fieldJdbcType = queryFieldType(nodeData.getOffsetFiledId());
+
+        EntityFieldDataReqDTO reqDTO = new EntityFieldDataReqDTO();
+        reqDTO.setEntityId(nodeData.getEntityId());
+        if (fieldJdbcType == JdbcTypeEnum.TIMESTAMP) {
+            nodeData.calculateOffTime(LocalDateTime.now());
+        }
+        if (fieldJdbcType == JdbcTypeEnum.DATE) {
+            nodeData.calculateOffTime(LocalDate.now());
+        }
+
+        if (CollectionUtils.isNotEmpty(nodeData.getFilterCondition())) {
+            List<Conditions> conditions = nodeData.getFilterCondition();
+            OrExpression orExpression = conditionsProvider.formatConditionsForValue(this, variableContext, inLoopDepth, conditions);
+            reqDTO.setConditionDTO(DataMethodApiHelper.processFilterCondition(orExpression));
+        }
+        if (nodeData.isBatchMode()) {
+            reqDTO.setNum(nodeData.getBatchSize());
+        }
+        List<List<EntityFieldDataRespDTO>> fieldDataRespDTOS = TenantUtils.executeIgnore(() -> dataMethodApi.getDataByCondition(reqDTO));
+        variableContext.putNodeVariables(this.getTag(), DataMethodApiHelper.convertToListMap(fieldDataRespDTOS));
+    }
+
+    private JdbcTypeEnum queryFieldType(Long filedId) {
+        EntityFieldJdbcTypeReqDTO reqDTO = new EntityFieldJdbcTypeReqDTO();
+        reqDTO.setFieldIds(List.of(filedId));
+        List<EntityFieldJdbcTypeRespDTO> respDTOS = metadataEntityFieldApi.getFieldJdbcTypes(reqDTO);
+        if (CollectionUtils.isNotEmpty(respDTOS)) {
+            EntityFieldJdbcTypeRespDTO respDTO = respDTOS.get(0);
+            return JdbcTypeEnum.getByCode(respDTO.getJdbcType());
+        } else {
+            throw new IllegalArgumentException("字段不存在:" + filedId);
+        }
     }
 
 }
--- onebase-module-flow/onebase-module-flow-context/src/main/java/com/cmsr/onebase/module/flow/context/graph/nodes/StartDateFieldNodeData.java
+++ onebase-module-flow/onebase-module-flow-context/src/main/java/com/cmsr/onebase/module/flow/context/graph/nodes/StartDateFieldNodeData.java
@@ -5,6 +5,8 @@ import com.cmsr.onebase.module.flow.context.graph.NodeData;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -18,14 +20,26 @@ public class StartDateFieldNodeData extends NodeData implements Serializable {
 
     private Long entityId;
 
+    /**
+     * 偏移字段ID
+     */
     private Long offsetFiledId;
 
+    /**
+     * 偏移模式  before  after
+     */
     private String offsetMode;
 
     private Integer offsetValue;
 
+    /**
+     * 偏移单位 day hour minute
+     */
     private String offsetUnit;
 
+    /**
+     * 每日执行时间  09:00
+     */
     private String dailyExecTime;
 
     private boolean batchMode;
@@ -41,6 +55,74 @@ public class StartDateFieldNodeData extends NodeData implements Serializable {
         Cron cron = new Cron();
         cron.setHourAndMinute(dailyExecTime);
         return cron.toCron();
+    }
+
+    /**
+     * 根据 offsetMode offsetValue offsetUnit 计算出 offsetTime
+     *
+     * @param triggerTime
+     * @return
+     */
+    public List<LocalDateTime> calculateOffTime(LocalDateTime triggerTime) {
+        if (triggerTime == null || offsetValue == null || offsetUnit == null || offsetMode == null) {
+            throw new IllegalArgumentException("参数错误");
+        }
+        LocalDateTime resultTime;
+        // 根据偏移模式和单位计算偏移时间
+        if ("before".equals(offsetMode)) {
+            // 在触发时间之后
+            switch (offsetUnit) {
+                case "day":
+                    resultTime = triggerTime.plusDays(offsetValue);
+                    break;
+                case "hour":
+                    resultTime = triggerTime.plusHours(offsetValue);
+                    break;
+                case "minute":
+                    resultTime = triggerTime.plusMinutes(offsetValue);
+                    break;
+                default:
+                    throw new IllegalArgumentException("未知的偏移单位:" + offsetUnit);
+            }
+        } else if ("after".equals(offsetMode)) {
+            // 在触发时间之前
+            switch (offsetUnit) {
+                case "day":
+                    resultTime = triggerTime.minusDays(offsetValue);
+                    break;
+                case "hour":
+                    resultTime = triggerTime.minusHours(offsetValue);
+                    break;
+                case "minute":
+                    resultTime = triggerTime.minusMinutes(offsetValue);
+                    break;
+                default:
+                    throw new IllegalArgumentException("未知的偏移单位:" + offsetUnit);
+            }
+        } else {
+            resultTime = triggerTime;
+        }
+        //resultTime 设置 时分秒为 00:00:00
+        LocalDateTime startTime = resultTime.withHour(0).withMinute(0).withSecond(0).withNano(0);
+        //resultTime 设置 时分秒为 23:59:59
+        LocalDateTime endTime = resultTime.withHour(23).withMinute(59).withSecond(59).withNano(999_999_999);
+        return List.of(startTime, endTime);
+    }
+
+    public LocalDate calculateOffTime(LocalDate triggerTime) {
+        if (triggerTime == null || offsetValue == null || offsetUnit == null || offsetMode == null) {
+            throw new IllegalArgumentException("参数错误");
+        }
+        LocalDate resultTime = triggerTime;
+        // 根据偏移模式和单位计算偏移时间
+        if ("before".equals(offsetMode)) {
+            resultTime.plusDays(offsetValue);
+        } else if ("after".equals(offsetMode)) {
+            resultTime.minusDays(offsetValue);
+        } else {
+            resultTime = triggerTime;
+        }
+        return resultTime;
     }
 
 }
