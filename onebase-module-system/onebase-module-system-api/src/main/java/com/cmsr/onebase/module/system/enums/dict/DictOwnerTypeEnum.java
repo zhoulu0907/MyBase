157065b416d972918a82c30af594a0b9813debce
biantianyu <919284750@qq.com>
2025-10-21 16:26:10 +0800

    SZHDZ-3329 feat(dict): 增加字典所有者类型和ID字段支持
    - 新增字典所有者类型枚举 DictOwnerTypeEnum
    - 在 DictTypeDO 中添加 dictOwnerType 和 dictOwnerId 字段
    - 更新 DictTypePageReqVO、DictTypeRespVO、DictTypeSaveReqVO- 在 DictTypeRepository 中增加所有者类型和ID的查询条件
    - 为 DictTypeServiceImpl 添加默认所有者类型设置逻辑
    - 所有相关 VO 类增加字典所有者字段以支持应用和租户区分- 支持按字典所有者类型和ID进行筛选和展示

---
+++ onebase-module-system/onebase-module-system-api/src/main/java/com/cmsr/onebase/module/system/enums/dict/DictOwnerTypeEnum.java
@@ -0,0 +1,68 @@
+package com.cmsr.onebase.module.system.enums.dict;
+
+import cn.hutool.core.util.ObjUtil;
+import com.cmsr.onebase.framework.common.core.ArrayValuable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+/**
+ * 字典所有者类型枚举
+ *
+ * @author bty418
+ * @date 2025-10-21
+ */
+@Getter
+@AllArgsConstructor
+public enum DictOwnerTypeEnum implements ArrayValuable<String> {
+
+    /**
+     * 应用自定义字典
+     */
+    APP("app", "应用自定义字典"),
+
+    /**
+     * 租户公共字典（空间公共字典）
+     */
+    TENANT("tenant", "空间公共字典");
+
+    public static final String[] ARRAYS = Arrays.stream(values()).map(DictOwnerTypeEnum::getType).toArray(String[]::new);
+
+    /**
+     * 类型值
+     */
+    private final String type;
+
+    /**
+     * 类型名称
+     */
+    private final String name;
+
+    @Override
+    public String[] array() {
+        return ARRAYS;
+    }
+
+    /**
+     * 判断是否为应用类型字典
+     *
+     * @param type 字典所有者类型
+     * @return 是否为应用类型
+     */
+    public static boolean isApp(String type) {
+        return ObjUtil.equal(APP.type, type);
+    }
+
+    /**
+     * 判断是否为租户类型字典
+     *
+     * @param type 字典所有者类型
+     * @return 是否为租户类型
+     */
+    public static boolean isTenant(String type) {
+        return ObjUtil.equal(TENANT.type, type);
+    }
+
+}
+
