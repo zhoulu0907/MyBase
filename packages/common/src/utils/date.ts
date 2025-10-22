93c7f26127f35b746eb4d62413ea93f860fc374f
chenyongqiang <chenyongqiang@richinfo.cn>
2025-10-22 10:14:49 +0800

    S25029301301-4 逻辑控制执行记录静态页面

--- packages/common/src/utils/date.ts
+++ packages/common/src/utils/date.ts
@@ -7,7 +7,7 @@ export const formatTimeYMDHMS = (timestamp: string | number | null | undefined):
   if (!timestamp) {
     return '--';
   }
-  
+
   const date = new Date(timestamp);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -15,6 +15,6 @@ export const formatTimeYMDHMS = (timestamp: string | number | null | undefined):
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
-  
+
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
