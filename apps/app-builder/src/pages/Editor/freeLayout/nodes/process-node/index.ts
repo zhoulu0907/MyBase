c1b2326f433c2407225426b8c13419635de78073
lihan <123>
2025-10-24 15:40:05 +0800

    S25029301301 删除不必要代码

--- apps/app-builder/src/pages/Editor/freeLayout/nodes/process-node/index.ts
+++ apps/app-builder/src/pages/Editor/freeLayout/nodes/process-node/index.ts
@@ -15,7 +15,6 @@ export const ProcessNodeRegistry: FlowNodeRegistry = {
   type: WorkflowNodeType.ProcessNode,
   meta: {
     defaultPorts: [{ type: 'input' }],
-    // sidebarDisabled: true
   },
   info: {
     icon: iconContinue,
