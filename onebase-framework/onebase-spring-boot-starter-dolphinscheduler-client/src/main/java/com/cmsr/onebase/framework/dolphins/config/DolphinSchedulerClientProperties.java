ff8bc32f5fe7a2ade17b2af445175aaf11ec9423
chengyuansen <chengyuansen@cmsr.chinamobile.com>
2025-10-24 17:21:04 +0800

    新增3个字段

--- onebase-framework/onebase-spring-boot-starter-dolphinscheduler-client/src/main/java/com/cmsr/onebase/framework/dolphins/config/DolphinSchedulerClientProperties.java
+++ onebase-framework/onebase-spring-boot-starter-dolphinscheduler-client/src/main/java/com/cmsr/onebase/framework/dolphins/config/DolphinSchedulerClientProperties.java
@@ -17,23 +17,32 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "onebase.dolphinscheduler")
 public class DolphinSchedulerClientProperties {
 
-  /** DolphinScheduler 基础地址，必须以 / 结尾，例如：http://127.0.0.1:12345/dolphinscheduler/ */
-  @NotBlank(message = "baseUrl 不能为空")
-  private String baseUrl;
+    /** DolphinScheduler 基础地址，必须以 / 结尾，例如：http://127.0.0.1:12345/dolphinscheduler/ */
+    @NotBlank(message = "baseUrl 不能为空")
+    private String baseUrl;
 
-  /** 访问认证 token */
-  @NotBlank(message = "token 不能为空")
-  private String token;
+    /** 访问认证 token */
+    @NotBlank(message = "token 不能为空")
+    private String token;
 
-  /** 连接超时时间，默认 5s */
-  private Duration connectTimeout = Duration.ofSeconds(10);
+    /** 工作流用projectCode */
+    private String projectCodeFlow;
 
-  /** 读取超时时间，默认 30s */
-  private Duration readTimeout = Duration.ofSeconds(30);
+    /** ETL用projectCode */
+    private String projectCodeETL;
 
-  /** 写入超时时间，默认 30s */
-  private Duration writeTimeout = Duration.ofSeconds(30);
+    /** 租户code */
+    private String tenantCode;
 
-  /** 日志级别：TRACE/DEBUG/INFO/WARN/ERROR，默认 INFO */
-  private String logLevel = "INFO";
+    /** 连接超时时间，默认 5s */
+    private Duration connectTimeout = Duration.ofSeconds(10);
+
+    /** 读取超时时间，默认 30s */
+    private Duration readTimeout = Duration.ofSeconds(30);
+
+    /** 写入超时时间，默认 30s */
+    private Duration writeTimeout = Duration.ofSeconds(30);
+
+    /** 日志级别：TRACE/DEBUG/INFO/WARN/ERROR，默认 INFO */
+    private String logLevel = "INFO";
 }
