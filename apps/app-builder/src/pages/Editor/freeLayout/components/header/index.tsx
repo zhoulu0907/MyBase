027e07487eabf6d26dd7edd1cd6f0f31569ab93e
lihan <123>
2025-10-31 16:40:33 +0800

    S25029301301-672 样式修改

--- apps/app-builder/src/pages/Editor/freeLayout/components/header/index.tsx
+++ apps/app-builder/src/pages/Editor/freeLayout/components/header/index.tsx
@@ -3,27 +3,65 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { useContext } from 'react';
-
+import { useContext, useState } from 'react';
+import { Input } from '@arco-design/web-react';
 import { getIcon } from './utils';
 import { useNodeRenderContext } from '../../hooks';
 import close from '../../assets/close.svg';
 import edit from '../../assets/edit.svg';
 import styles from './index.module.less';
 import { SidebarContext } from '../../context';
-
+import { type NodeRenderReturnType } from '@flowgram.ai/free-layout-editor';
+interface HeaderProps {
+  nodeRender: NodeRenderReturnType;
+}
 export default function Header() {
+  // export default function Header({ nodeRender }: HeaderProps) {
   const { node, data } = useNodeRenderContext();
   const { setNodeId } = useContext(SidebarContext);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(data.name);
   const handleClose = () => {
     setNodeId(undefined);
+  };
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditValue(data.name);
+  };
+
+  const handleSave = () => {
+    // // setData({ ...data, name: editValue });
+    // nodeRender?.form?.setValueIn('name', editValue);
+    // setIsEditing(false);
+    // console.log(nodeRender);
+  };
+
+  const handleCancel = () => {
+    setEditValue(data.name);
+    setIsEditing(false);
   };
   return (
     <div className={styles.sidebarHeader}>
       <div className={styles.leftTitle}>
         <div className={styles.icon}> {getIcon(node)}</div>
-        <div className={styles.title}>{data.name}</div>
-        <img className={styles.edit} src={edit} />
+        {isEditing ? (
+          <Input
+            value={editValue}
+            onChange={setEditValue}
+            onBlur={handleSave}
+            onPressEnter={handleSave}
+            onKeyDown={(e) => {
+              if (e.key === 'Escape') {
+                handleCancel();
+              }
+            }}
+            className={styles.titleInput}
+            autoFocus
+          />
+        ) : (
+          <div className={styles.title}>{data.name}</div>
+        )}
+        {!isEditing && <img className={styles.edit} src={edit} onClick={handleEdit} />}
       </div>
       <img className={styles.close} src={close} alt="" onClick={handleClose} />
     </div>
