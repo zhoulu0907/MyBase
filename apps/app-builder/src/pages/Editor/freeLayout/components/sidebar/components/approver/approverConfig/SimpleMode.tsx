d44d941ef24b3f808385329061af5fefd9aaf5da
ext_ganjinchun <779451699@qq.com>
2025-10-31 11:09:20 +0800

    S25029301301-621 审批人抽屉，增加用户列表、角色列表接口

--- apps/app-builder/src/pages/Editor/freeLayout/components/sidebar/components/approver/approverConfig/SimpleMode.tsx
+++ apps/app-builder/src/pages/Editor/freeLayout/components/sidebar/components/approver/approverConfig/SimpleMode.tsx
@@ -3,13 +3,22 @@ import { Radio, Form, Select } from '@arco-design/web-react';
 import { IconQuestionCircle } from '@arco-design/web-react/icon';
 import styles from './index.module.less';
 import { type ApproverConfig } from '../constant';
+import type { PageParam } from '@onebase/platform-center';
+import { getUserPage } from '@onebase/platform-center';
+import { getAppIdByPageSetId, listRole, type ListRoleReq } from '@onebase/app';
+import { useLocation } from 'react-router-dom';
+
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
 const Option = Select.Option;
 
 const SimpleMode = ({ setApprovalConfigData, approverConfig }: ApproverConfig) => {
-  const [userOptions, setUserOptions] = useState([]);
-  const [roleOptions, setRoleOptions] = useState([]);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const pageSetId = searchParams.get('pageSetId') || '';
+
+  const [userOptions, setUserOptions] = useState<any[]>([]);
+  const [roleOptions, setRoleOptions] = useState<any[]>([]);
   const [simpleCkType, setSimpleCkType] = useState<string>(approverConfig?.approverType || 'user');
   const [form] = Form.useForm();
   // 校验规则
@@ -20,229 +29,53 @@ const SimpleMode = ({ setApprovalConfigData, approverConfig }: ApproverConfig) =
   const [formRes, setFormRes] = useState<any>({});
 
   function initUserData() {
-    let userRes = {
-      code: 0,
-      data: {
-        list: [
-          {
-            "id": "113389937034330112",
-            "username": "liyang",
-            "nickname": "李洋",
-            "deptId": "114",
-            "deptName": "测试一级",
-            "email": "",
-            "mobile": "15011111111",
-            "sex": 0,
-            "avatar": "",
-            "status": 1,
-            "loginIp": "",
-            "createTime": 1761787363630,
-            "adminType": 2
-          },
-          {
-            id: '101802183959412736',
-            username: 'gaoguoqing',
-            nickname: '高国清',
-            deptId: '545473992555892736',
-            deptName: '科创中心',
-            email: 'zhentao789@126.com',
-            mobile: '15890615800',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '0:0:0:0:0:0:0:1',
-            loginDate: 1761294924937,
-            createTime: 1761112867155,
-            adminType: 1
-          },
-          {
-            id: '1',
-            username: 'admin',
-            nickname: 'admin',
-            remark: '管理员',
-            deptId: '1',
-            deptName: '222',
-            postIds: ['1'],
-            email: 'aoteman@1226.com',
-            mobile: '18818260272',
-            sex: 2,
-            avatar: 'http://test.yudao.iocoder.cn/96c787a2ce88bf6d0ce3cd8b6cf5314e80e7703cd41bf4af8cd2e2909dbd6b6d.png',
-            status: 1,
-            loginIp: '10.0.104.23',
-            loginDate: 1761706784448,
-            createTime: 1609837427000,
-            adminType: 1
-          },
-          {
-            id: '104558951679918080',
-            username: 'user10001',
-            nickname: 'admin123',
-            email: '111111@126.com',
-            mobile: '15890615800',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1761273332532,
-            adminType: 2
-          },
-          {
-            id: '104536566310174720',
-            username: '10000',
-            nickname: 'admin123',
-            email: '111111@126.com',
-            mobile: '15890615800',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1761272029727,
-            adminType: 2
-          },
-          {
-            id: '92907220776845312',
-            username: 'newliu',
-            nickname: '新柳',
-            mobile: '19099998888',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760595112956,
-            adminType: 2
-          },
-          {
-            id: '91573633428258816',
-            username: 'adminzjx',
-            nickname: 'ZJX',
-            deptId: '100',
-            deptName: '一级部门',
-            mobile: '18616337513',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760517487392,
-            adminType: 2
-          },
-          {
-            id: '91163120454107136',
-            username: 'wangjie',
-            nickname: '王杰',
-            email: '',
-            mobile: '18771963538',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '10.0.104.23',
-            loginDate: 1760578017132,
-            createTime: 1760493592517,
-            adminType: 2
-          },
-          {
-            id: '85546007599284224',
-            username: 'chenyongqiang',
-            nickname: '陈永强',
-            deptId: '889796964974590',
-            deptName: 'onebase',
-            email: '',
-            mobile: '17862966370',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760166633871,
-            adminType: 2
-          },
-          {
-            id: '84165295868870656',
-            username: 'zchtest06',
-            nickname: 'zchtest06',
-            email: '',
-            mobile: '15000588036',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760086265839,
-            adminType: 2
-          },
-          {
-            id: '84164832012402688',
-            username: 'zchtest05',
-            nickname: 'zchtest05',
-            email: '',
-            mobile: '15000588035',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760086238304,
-            adminType: 2
-          }
-        ],
-        total: '65'
-      },
-      msg: ''
+    const params: PageParam = {
+      pageNo: 1,
+      pageSize: 100
     };
-    let select_arr: any = [];
-    userRes.data.list.forEach((item: any) => {
-      select_arr.push({
-        userId: item.id,
-        name: item.nickname
-      });
-    });
-    setUserOptions(select_arr);
+    getUserPage(params).then((res:any) => {
+      if (Array.isArray(res?.list)) {
+        let selectArr: any[] = [];
+        res.list.forEach((item: any) => {
+          selectArr.push({
+            userId: item.id,
+            name: item.nickname
+          });
+        });
+        setUserOptions(selectArr);
+      }
+    }).catch((err:any) => {
+      console.info('Api getUserPage Error:', err)
+    })
   }
   function initRoleData() {
-    let roleRes = {
-      code: 0,
-      data: {
-        list: [
-          {
-            id: '1',
-            username: 'admin',
-            nickname: 'admin',
-            remark: '管理员',
-            deptId: '1',
-            deptName: '222',
-            postIds: ['1'],
-            email: 'aoteman@1226.com',
-            mobile: '18818260272',
-            sex: 2,
-            avatar: 'http://test.yudao.iocoder.cn/96c787a2ce88bf6d0ce3cd8b6cf5314e80e7703cd41bf4af8cd2e2909dbd6b6d.png',
-            status: 1,
-            loginIp: '10.0.104.23',
-            loginDate: 1761706784448,
-            createTime: 1609837427000,
-            adminType: 1
-          },
-          {
-            id: '84164832012402688',
-            username: 'zchtest05',
-            nickname: 'zchtest05',
-            email: '',
-            mobile: '15000588035',
-            sex: 0,
-            avatar: '',
-            status: 1,
-            loginIp: '',
-            createTime: 1760086238304,
-            adminType: 2
+    if (!pageSetId) {
+      console.error('Api getAppIdByPageSetId param is Error.')
+      return;
+    }
+    getAppIdByPageSetId({ pageSetId }).then((appId:any) => {
+      if (appId) {
+        const params: ListRoleReq = {
+          applicationId: appId
+        };
+        listRole(params).then((roleRes:any) => {
+          if (Array.isArray(roleRes)) {
+            let selectArr: any[] = [];
+            roleRes.forEach((item: any) => {
+              selectArr.push({
+                roleId: item.id,
+                roleName: item.roleName
+              });
+            });
+            setRoleOptions(selectArr);
           }
-        ],
-        total: '65'
-      },
-      msg: ''
-    };
-    let select_arr: any = [];
-    roleRes.data.list.forEach((item: any) => {
-      select_arr.push({
-        roleId: item.id,
-        roleName: item.nickname
-      });
-    });
-    setRoleOptions(select_arr);
+        }).catch((err:any) => {
+          console.error('Api listRole Error:', err)
+        })
+      }
+    }).catch((err:any) => {
+      console.error('Api getAppIdByPageSetId Error:', err)
+    })
   }
 
   function changeSimpleType(val: string) {
