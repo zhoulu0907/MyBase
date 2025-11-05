880dbb36cccaa1aa8b119f052930f6bcff83449c
liyang <liyang@cmsr.chinamobile.com>
2025-11-05 17:53:04 +0800

    S25029301301-680 BPM流程版本列表参数增加校验

--- onebase-module-bpm/onebase-module-bpm-build/src/main/java/com/cmsr/onebase/module/bpm/build/vo/vermgmt/BpmVersionMgmtPageReqVo.java
+++ onebase-module-bpm/onebase-module-bpm-build/src/main/java/com/cmsr/onebase/module/bpm/build/vo/vermgmt/BpmVersionMgmtPageReqVo.java
@@ -2,10 +2,9 @@ package com.cmsr.onebase.module.bpm.build.vo.vermgmt;
 
 import com.cmsr.onebase.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
-import java.io.Serializable;
 
 /**
  * 获取流程版本管理列表VO
@@ -18,7 +17,7 @@ public class BpmVersionMgmtPageReqVo extends PageParam {
 
     @Schema(description = "表单ID", required = true, example = "113771690916872193")
     @NotNull(message = "表单ID不能为空")
-    private String businessId;
+    private Long businessId;
 
     @Schema(description = "流程版本备注")
     private String versionAlias;
@@ -28,5 +27,6 @@ public class BpmVersionMgmtPageReqVo extends PageParam {
 
     @Schema(description = "排序方式：update_time-按更新时间排序, create_time-按创建时间排序",
             example = "update_time", defaultValue = "update_time")
+    @NotBlank(message = "排序方式不能为空")
     private String sortType;
 }
