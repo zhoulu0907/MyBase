4c7c7a9be5b77d4aa3624ea1e4532cdaf065e7b3
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-11-05 10:36:45 +0800

    初始化实体类

---
+++ onebase-module-data/onebase-module-etl-executor/src/main/java/com/cmsr/onebase/module/etl/executor/entity/FieldDefine.java
@@ -0,0 +1,14 @@
+package com.cmsr.onebase.module.etl.executor.entity;
+
+import lombok.Data;
+
+@Data
+public class FieldDefine {
+
+    private String fieldId;
+
+    private String fieldName;
+
+    private String fieldType;
+
+}
