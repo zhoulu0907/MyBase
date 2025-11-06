10f9e51d685e54ab8f8c9d990768335999eb5b4a
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-11-06 17:29:30 +0800

    S25029301301-769 dev

--- apps/app-builder/src/pages/ETLFlowEditor/configs/input/inputNode.tsx
+++ apps/app-builder/src/pages/ETLFlowEditor/configs/input/inputNode.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@arco-design/web-react';
+import { Button, Input } from '@arco-design/web-react';
 import { ETLDrawerTab, etlEditorSignal } from '@onebase/common';
 import { useSignals } from '@preact/signals-react/runtime';
 import React from 'react';
@@ -13,6 +13,23 @@ export const InputNodeConfig: React.FC = () => {
 
   return (
     <div className={styles.config}>
+      {curDrawerTab.value === ETLDrawerTab.DATA_PREVIEW && (
+        <div className={styles.dataPreview}>
+          <div className={styles.dataSource}>
+            <div className={styles.dataSourceHeader}>
+              <div className={styles.dataSourceHeaderLeft}>输入源</div>
+              <div className={styles.dataSourceHeaderRight}>
+                <Button type="text">更改输入源</Button>
+              </div>
+            </div>
+            <div className={styles.dataSourceContent}>
+              <div className={styles.dataSourceName}>数据源名称</div>
+              <div className={styles.selectedFields}>已选字段</div>
+            </div>
+          </div>
+          <div className={styles.dataPreviewContent}></div>
+        </div>
+      )}
       {curDrawerTab.value === ETLDrawerTab.NODE_REMARK && (
         <TextArea placeholder="请输入节点备注" autoSize={{ minRows: 3, maxRows: 6 }} allowClear />
       )}
