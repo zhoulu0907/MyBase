e4578c64953f31a2abe94549ac8e9d30beaf04dc
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-11-13 14:31:24 +0800

    初始化Jose代码

--- onebase-framework/onebase-jose/src/main/java/com/cmsr/onebase/jose/JoseGenerator.java
+++ onebase-framework/onebase-jose/src/main/java/com/cmsr/onebase/jose/JoseGenerator.java
@@ -1,5 +1,56 @@
 package com.cmsr.onebase.jose;
 
-public class JoseGenerator {
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.crypto.RSAEncrypter;
+import com.nimbusds.jwt.EncryptedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+import java.util.UUID;
+
+public class JoseGenerator {
+    private static final String PUBLIC_KEY_PEM = "/onebase_pub.pem";
+
+    public static String generateToken(int expireSeconds) throws Exception {
+
+        try (InputStream resourceInputStream = JoseGenerator.class.getResourceAsStream(PUBLIC_KEY_PEM);
+             InputStreamReader inputStreamReader = new InputStreamReader(resourceInputStream)
+        ) {
+            PEMParser pemParser = new PEMParser(inputStreamReader);
+            SubjectPublicKeyInfo publicKeyInfo = (SubjectPublicKeyInfo) pemParser.readObject();
+
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+            RSAPublicKey publicKey = (RSAPublicKey) converter.getPublicKey(publicKeyInfo);
+
+            JWTClaimsSet.Builder claimSetBuilder = new JWTClaimsSet.Builder();
+            claimSetBuilder.issuer("onebase");
+            claimSetBuilder.subject("onebase");
+
+            claimSetBuilder.expirationTime(new Date(new Date().getTime() + 1000L * expireSeconds));
+            claimSetBuilder.notBeforeTime(new Date());
+            claimSetBuilder.jwtID(UUID.randomUUID().toString());
+            //
+            JWEHeader jweHeader = new JWEHeader(JWEAlgorithm.RSA_OAEP_512, EncryptionMethod.A192CBC_HS384);
+            EncryptedJWT jwt = new EncryptedJWT(jweHeader, claimSetBuilder.build());
+
+            RSAEncrypter rsaEncrypter = new RSAEncrypter(publicKey);
+            jwt.encrypt(rsaEncrypter);
+
+            return jwt.serialize();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String token = generateToken(30);
+
+        System.out.println("${setValue(accessToken=" + token + ")}");
+    }
 }
--- onebase-framework/onebase-jose/src/main/java/com/cmsr/onebase/jose/JoseValidator.java
+++ onebase-framework/onebase-jose/src/main/java/com/cmsr/onebase/jose/JoseValidator.java
@@ -1,4 +1,43 @@
 package com.cmsr.onebase.jose;
 
+import com.nimbusds.jose.crypto.RSADecrypter;
+import com.nimbusds.jwt.EncryptedJWT;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.PrivateKey;
+import java.util.Date;
+
 public class JoseValidator {
+    private static final String PRIVATE_KEY_PEM = "/onebase_pri.pem";
+
+    public static boolean validate(String token) throws Exception {
+        Date now = new Date();
+
+        EncryptedJWT jwt = EncryptedJWT.parse(token);
+
+        try (InputStream resourceStream = JoseValidator.class.getResourceAsStream(PRIVATE_KEY_PEM);
+             InputStreamReader resourceInputStreamReader = new InputStreamReader(resourceStream)
+        ) {
+            PEMParser pemParser = new PEMParser(resourceInputStreamReader);
+            PEMKeyPair pemKeyPair = (PEMKeyPair) pemParser.readObject();
+            PrivateKeyInfo privateKeyInfo = pemKeyPair.getPrivateKeyInfo();
+
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+            PrivateKey privateKey = converter.getPrivateKey(privateKeyInfo);
+            RSADecrypter decrypter = new RSADecrypter(privateKey);
+            jwt.decrypt(decrypter);
+
+            Date expirationTime = jwt.getJWTClaimsSet().getExpirationTime();
+            if (expirationTime.before(now)) {
+                return false;
+            }
+            return true;
+        }
+    }
+
 }
