8ec19923bb0f41ab0ceb4040c3813b913f3ff57b
jinyonghu <jinyonghu@cmsr.chinamobile.com>
2025-11-19 16:41:32 +0800

    S25029301301-752 app-build 编译问题

--- apps/app-builder/src/pages/Editor/pages/FromEditor/formEditor.tsx
+++ apps/app-builder/src/pages/Editor/pages/FromEditor/formEditor.tsx
@@ -1,150 +1,17 @@
-import { loadMicroApp, type MicroApp } from 'qiankun';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import EditorConfig from '../../components/config';
-// import EditorPanel from '../../components/panel/Panel';
-import globalState from '@/store/microService/state';
-import { EditMode } from '@onebase/common';
-import { currentEditorSignal, EditorPanel, usePageEditorSignal, usePageViewEditorSignal } from '@onebase/ui-kit';
-import { useSignals } from '@preact/signals-react/runtime';
+import EditorPanel from '../../components/panel/Panel';
 import EditorWorkspace from '../../components/workspace/Workspace';
+
 import styles from './index.module.less';
 
 const FormEditor: React.FC = () => {
-  useSignals();
-
-  const { editMode, setEditMode, curComponentID, setCurComponentID, clearCurComponentID, setCurComponentSchema } =
-    currentEditorSignal;
-  const {
-    components,
-    setComponents,
-    addComponents,
-    delComponents,
-    showDeleteButton,
-    setShowDeleteButton,
-    layoutSubComponents,
-    setLayoutSubComponents,
-    delLayoutSubComponents
-  } = usePageEditorSignal();
-  const { pageComponentSchemas, setPageComponentSchemas, delPageComponentSchemas } = usePageEditorSignal();
-  const { pageViews, curViewId, setCurViewId, updatePageViewName } = usePageViewEditorSignal;
-
-  const microAppRef = useRef<MicroApp | null>(null);
-
-  // 监听全局状态变化
-  useEffect(() => {
-    globalState.onGlobalStateChange(
-      (state: any, prev: any) => {
-        console.log('主应用接收到全局状态变化', state, prev);
-      },
-      true // 立即执行一次
-    );
-
-    // 返回清理函数
-    return () => {
-      globalState.offGlobalStateChange();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (editMode.value === EditMode.MOBILE) {
-      const microApp = loadMicroApp(
-        {
-          name: 'mobile-editor',
-          entry: (window as any).global_config?.MOBILE_EDITOR_URL,
-          container: '#mobile-editor',
-          props: {
-            onGlobalStateChange: globalState.onGlobalStateChange,
-            setGlobalState: globalState.setGlobalState,
-            offGlobalStateChange: globalState.offGlobalStateChange
-          }
-        },
-        {
-          sandbox: {
-            experimentalStyleIsolation: true
-          }
-        }
-      );
-      microAppRef.current = microApp;
-
-      return () => {
-        microApp?.unmount();
-      };
-    } else {
-      microAppRef.current?.unmount();
-    }
-  }, [editMode.value]);
-
-  useEffect(() => {
-    console.log('components: ', components);
-
-    globalState.setGlobalState({
-      editMode: editMode.value,
-      setEditMode: setEditMode,
-
-      curComponentID: curComponentID.value,
-      setCurComponentID: setCurComponentID,
-      clearCurComponentID: clearCurComponentID,
-      setCurComponentSchema: setCurComponentSchema,
-
-      pageComponentSchemas: pageComponentSchemas,
-      setPageComponentSchemas: setPageComponentSchemas,
-      delPageComponentSchemas: delPageComponentSchemas,
-
-      components: components,
-      setComponents: setComponents,
-      addComponents: addComponents,
-      delComponents: delComponents,
-
-      showDeleteButton: showDeleteButton,
-      setShowDeleteButton: setShowDeleteButton,
-
-      layoutSubComponents: layoutSubComponents,
-      setLayoutSubComponents: setLayoutSubComponents,
-      delLayoutSubComponents: delLayoutSubComponents,
-
-      pageViews: pageViews.value,
-      curViewId: curViewId.value,
-      setCurViewId: setCurViewId,
-      updatePageViewName: updatePageViewName
-    });
-  }, [
-    editMode.value,
-    curComponentID.value,
-    pageComponentSchemas,
-    components,
-    showDeleteButton,
-    layoutSubComponents,
-    pageViews.value,
-    curViewId.value
-  ]);
-
   return (
-    <>
-      <div className={styles.formEditorPage}>
-        <div
-          className={styles.leftContainer}
-          style={{
-            display: editMode.value == EditMode.PC ? 'flex' : 'none'
-          }}
-        >
-          <EditorPanel />
-          <EditorWorkspace />
-        </div>
-
-        <div
-          className={styles.leftContainer}
-          style={{
-            display: editMode.value == EditMode.MOBILE ? 'flex' : 'none'
-          }}
-        >
-          <div id="mobile-editor"></div>
-        </div>
-
-        <div className={styles.rightContainer}>
-          <EditorConfig />
-        </div>
-      </div>
-    </>
+    <div className={styles.formEditorPage}>
+      <EditorPanel />
+      <EditorWorkspace />
+      <EditorConfig />
+    </div>
   );
 };
 
