f12d81a406af17e835dd767cbe78f8febc3e1dae
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-11-20 16:49:11 +0800

    提交MybatisFlex

---
+++ onebase-framework/onebase-project-base/src/main/java/com/cmsr/onebase/framework/base/listener/BaseDOListener.java
@@ -0,0 +1,31 @@
+package com.cmsr.onebase.framework.base.listener;
+
+import com.cmsr.onebase.framework.data.base.BaseDO;
+import com.cmsr.onebase.framework.web.core.util.WebFrameworkUtils;
+import com.mybatisflex.annotation.InsertListener;
+import com.mybatisflex.annotation.UpdateListener;
+
+import java.time.LocalDateTime;
+
+public class BaseDOListener implements InsertListener, UpdateListener {
+
+    @Override
+    public void onInsert(Object o) {
+        if (o instanceof BaseDO baseDO) {
+            Long userId = WebFrameworkUtils.getLoginUserId();
+            baseDO.setCreator(userId);
+            baseDO.setUpdater(userId);
+            LocalDateTime now = LocalDateTime.now();
+            baseDO.setCreateTime(now);
+            baseDO.setUpdateTime(now);
+        }
+    }
+
+    @Override
+    public void onUpdate(Object o) {
+        if (o instanceof BaseDO baseDO) {
+            baseDO.setUpdater(WebFrameworkUtils.getLoginUserId());
+            baseDO.setUpdateTime(LocalDateTime.now());
+        }
+    }
+}
