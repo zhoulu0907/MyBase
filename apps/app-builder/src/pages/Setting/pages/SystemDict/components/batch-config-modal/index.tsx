9647df7d7167d76602ce48eb6a7dae739a996c1c
yangxiaoyi <yangxiaoyi@cmsr.chinamobile.com>
2025-11-21 11:08:00 +0800

    S25029301301-1418 新建字典值占位符修改

--- apps/app-builder/src/pages/Setting/pages/SystemDict/components/batch-config-modal/index.tsx
+++ apps/app-builder/src/pages/Setting/pages/SystemDict/components/batch-config-modal/index.tsx
@@ -300,7 +300,7 @@ const BatchConfigModal: React.FC<BatchConfigModalProps> = ({ visible, onCancel, 
         </>
       ),
       dataIndex: 'value',
-      width: 200,
+      width: 250,
       render: (_: unknown, record: DictData, index: number) => {
         const fieldName = `dictValues.${index}.value`;
         return (
@@ -312,7 +312,7 @@ const BatchConfigModal: React.FC<BatchConfigModalProps> = ({ visible, onCancel, 
             validateStatus={externalErrors[fieldName] ? 'error' : undefined}
             help={externalErrors[fieldName]}
           >
-            <Input placeholder="请输入字典值编码" value={record.value} />
+            <Input placeholder="请输入字母、数字或下划线" value={record.value} />
           </Form.Item>
         );
       }
