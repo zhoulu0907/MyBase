4a6431633d28f8c87f406f381b2f512624e463fd
matianyu <litesuits@qq.com>
2025-11-24 18:38:03 +0800

    developerPermissionCodes

--- onebase-module-system/onebase-module-system-api/src/main/java/com/cmsr/onebase/module/system/enums/permission/RoleCodeEnum.java
+++ onebase-module-system/onebase-module-system-api/src/main/java/com/cmsr/onebase/module/system/enums/permission/RoleCodeEnum.java
@@ -1,14 +1,12 @@
 package com.cmsr.onebase.module.system.enums.permission;
 
 import com.cmsr.onebase.framework.common.util.object.ObjectUtils;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * 角色标识枚举
  */
 @Getter
-@AllArgsConstructor
 public enum RoleCodeEnum {
 
     SUPER_ADMIN("super_admin", "平台管理员"),
@@ -16,6 +14,12 @@ public enum RoleCodeEnum {
     CORP_ADMIN("corp_admin", "企业管理员"),
     APP_DEVELOPER("app_developer", "应用开发者"),
     ;
+
+
+    RoleCodeEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 
     /**
      * 角色编码
@@ -25,6 +29,15 @@ public enum RoleCodeEnum {
      * 名字
      */
     private final String name;
+
+    /**
+     * 应用开发者权限编码列表(建议未来迁移到数据库配置)
+     */
+    private final String[] developerPermissionCodes = new String[]{
+            "",
+            "",
+            ""
+    };
 
     public static boolean isSuperAdmin(String code) {
         return ObjectUtils.equalsAny(code, SUPER_ADMIN.getCode());
