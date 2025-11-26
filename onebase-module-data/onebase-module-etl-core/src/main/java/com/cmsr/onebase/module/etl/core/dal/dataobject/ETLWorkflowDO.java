d29b996392eef5e28e9340d5232c60817f4cfe54
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-11-26 18:46:24 +0800

    hotfix

--- onebase-module-data/onebase-module-etl-core/src/main/java/com/cmsr/onebase/module/etl/core/dal/dataobject/ETLWorkflowDO.java
+++ onebase-module-data/onebase-module-etl-core/src/main/java/com/cmsr/onebase/module/etl/core/dal/dataobject/ETLWorkflowDO.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Table("etl_workflow")
 public class ETLWorkflowDO extends BaseAppEntity {
 
-    @Column("workflowUuid")
+    @Column("workflow_uuid")
     private String workflowUuid;
 
     @Column("workflow_name")
