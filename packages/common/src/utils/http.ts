3c8bb5849667da4473c6534e7547252416b40dd6
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-11-26 09:03:30 +0800

    S25029301301-737 dev

--- packages/common/src/utils/http.ts
+++ packages/common/src/utils/http.ts
@@ -63,7 +63,7 @@ export class HttpClient {
         if (tokenInfo?.accessToken) {
           config.headers['Authorization'] = `Bearer ${tokenInfo.accessToken}`;
 
-          //   config.headers['X-Tenant-Id'] = tenantInfo?.tenantId;
+          config.headers['X-Tenant-Id'] = tenantInfo?.tenantId;
         }
 
         // 执行自定义请求拦截器
