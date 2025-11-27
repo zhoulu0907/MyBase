fff08524098fd743c8de8ec71b2fc05c87825420
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-11-27 17:34:25 +0800

    添加工具类 UuidUtils

--- onebase-framework/onebase-common/src/main/java/com/cmsr/onebase/framework/common/util/string/UuidUtils.java
+++ onebase-framework/onebase-common/src/main/java/com/cmsr/onebase/framework/common/util/string/UuidUtils.java
@@ -3,7 +3,7 @@ package com.cmsr.onebase.framework.common.util.string;
 import com.github.f4b6a3.uuid.UuidCreator;
 
 public class UuidUtils {
-    public static String fetchUuid() {
+    public static String getUuid() {
         return UuidCreator.getTimeOrderedEpoch().toString();
     }
 }
