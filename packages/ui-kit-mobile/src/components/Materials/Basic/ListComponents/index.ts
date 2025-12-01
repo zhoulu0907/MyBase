6e339af0e921e6a2b4a20ab7f4cbc875e69795d7
jinyonghu <jinyonghu@cmsr.chinamobile.com>
2025-12-01 15:10:27 +0800

    S25029301301-752 类型问题调整

--- packages/ui-kit-mobile/src/components/Materials/Basic/ListComponents/index.ts
+++ packages/ui-kit-mobile/src/components/Materials/Basic/ListComponents/index.ts
@@ -2,8 +2,12 @@ import XShowMonitor from './ShowMonitor';
 import XLoadMore from './LoadMore';
 import XCarousel from './Carousel';
 
-export const ListComp = {
+export const ListComp: any = {
   XShowMonitor,
   XLoadMore,
   XCarousel
 };
+
+export { XShowMonitor, XLoadMore, XCarousel };
+
+export type ListComponentType = typeof ListComp;
