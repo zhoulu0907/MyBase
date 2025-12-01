70ce59addb740064fbb668894615b1fd8cf897a4
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-12-01 12:58:44 +0800

    S25029301301-737 add docker image

--- deploy/config/console_config.js
+++ deploy/config/console_config.js
@@ -1,9 +1,6 @@
 window.global_config = {
-  // BASE_URL: 'http://localhost:9524',
-  ENVIRONMENT: 'platform',
-  BASE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v1-snapshot/onebaseserver/admin-api',
-  PLATFORM_BASE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v1-snapshot/onebaseserver/platform',
-  PLATFORM_FE_URL: 'http://localhost:4399'
-//   PLATFORM_FE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v0/obbuildermt/'
-  // BASE_URL: 'http://192.168.43.40:48080/admin-api'
-};
+    ENVIRONMENT: 'platform',
+    BASE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v1-snapshot/onebaseserver/admin-api',
+    PLATFORM_BASE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v1-snapshot/onebaseserver/platform',
+    PLATFORM_FE_URL: 'http://s25029301301.dev.internal.virtueit.net:81/v0/obbuildermt/'
+}
