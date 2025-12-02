e35c6c199be0955b6cf1fab75b8f25fe59fe9c50
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-12-02 14:46:40 +0800

    S25029301301-3218 重复代码清理 重构

--- packages/ui-kit-mobile/tsup.config.ts
+++ packages/ui-kit-mobile/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   dts: true,
   splitting: true,
   sourcemap: true,
