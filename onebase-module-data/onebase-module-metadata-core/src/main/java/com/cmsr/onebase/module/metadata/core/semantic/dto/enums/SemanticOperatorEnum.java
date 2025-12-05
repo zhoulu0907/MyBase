0db0e10d890cd6705cc3101b453e2d15a7531292
xiayuanming <xiayuanming@cmsr.chinamobile.com>
2025-12-05 21:21:48 +0800

    修改DataMethodApiHelper适配新的SemanticConditionDTO

--- onebase-module-data/onebase-module-metadata-core/src/main/java/com/cmsr/onebase/module/metadata/core/semantic/dto/enums/SemanticOperatorEnum.java
+++ onebase-module-data/onebase-module-metadata-core/src/main/java/com/cmsr/onebase/module/metadata/core/semantic/dto/enums/SemanticOperatorEnum.java
@@ -9,6 +9,23 @@ public enum SemanticOperatorEnum {
     LE,
     LIKE,
     IN,
-    NIN
+    NIN,
+
+    // 以下为补充内容
+
+    // 不相似
+    NOT_LIKE,
+    // 为空
+    EMPTY,
+    // 不为空
+    NOT_EMPTY,
+    // 包含全部，要求所有元素均存在
+    CONTAINS_ALL,
+    // 不包含全部，要求所有元素均不存在
+    NOT_CONTAINS_ALL,
+    // 任一元素存在即可
+    CONTAINS_ANY,
+    // 任一元素均不可存在
+    NOT_CONTAINS_ANY
 }
 
