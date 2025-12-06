19a1f162c979d4493c3cab576aa0cd9ef29ca40b
gaoguoqing_lctx <zhentao789@126.com>
2025-12-06 11:30:53 +0800

    新增运行态部门列表接口2

--- onebase-module-system/onebase-module-system-runtime/src/main/java/com/cmsr/onebase/module/system/runtime/controller/dept/RuntimeDeptController.java
+++ onebase-module-system/onebase-module-system-runtime/src/main/java/com/cmsr/onebase/module/system/runtime/controller/dept/RuntimeDeptController.java
@@ -1,17 +1,11 @@
 package com.cmsr.onebase.module.system.runtime.controller.dept;
 
-import com.cmsr.onebase.framework.common.enums.CommonStatusEnum;
 import com.cmsr.onebase.framework.common.pojo.CommonResult;
-import com.cmsr.onebase.framework.common.util.object.BeanUtils;
-import com.cmsr.onebase.module.system.dal.dataobject.dept.DeptDO;
 import com.cmsr.onebase.module.system.service.dept.DeptService;
 import com.cmsr.onebase.module.system.vo.dept.*;
-import com.cmsr.onebase.module.system.vo.user.UserAdminOrDirectorUpdateReqVO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +17,8 @@ import static com.cmsr.onebase.framework.common.pojo.CommonResult.success;
 /**
  * 管理后台 - 部门 Controller
  *
- * @author matianyu
- * @date 2025-01-27
+ * @author
+ * @date 2025-12-05
  */
 @Tag(name = "管理后台 - 部门")
 @RestController
@@ -34,7 +28,6 @@ public class RuntimeDeptController {
 
     @Resource
     private DeptService deptService;
-
 
     @GetMapping("/list")
     @Operation(summary = "获取部门列表")
