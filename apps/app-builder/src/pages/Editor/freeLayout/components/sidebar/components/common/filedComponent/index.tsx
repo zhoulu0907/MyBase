c5fa18fdf2e76ca2274260774614f2e22d386e79
lihan <123>
2025-12-06 16:19:58 +0800

    S25029301301-2013 抄送人接口参数调整

--- apps/app-builder/src/pages/Editor/freeLayout/components/sidebar/components/common/filedComponent/index.tsx
+++ apps/app-builder/src/pages/Editor/freeLayout/components/sidebar/components/common/filedComponent/index.tsx
@@ -14,9 +14,8 @@ import './style.less';
  * @param columnsTable 表格配置
  */
 
-
 const FieldTable = forwardRef(
-  ({ onTableChange, title,tbData, setTableData, ckOptions, invert, columnsTable }: any, ref) => {
+  ({ onTableChange, title, tbData, setTableData, ckOptions, invert, columnsTable }: any, ref) => {
     // keyArr是专门给FieldModal弹窗用的，帮助弹窗反选
     const [curKeyArr, setCurKeyArr] = useState<any[]>([]);
     const [selectRowkeyArr, setSelectRowKeyArr] = useState([]);
@@ -25,7 +24,7 @@ const FieldTable = forwardRef(
       {
         title: '操作',
         width: 95,
-        dataIndex: 'fieldUuid',
+        dataIndex: 'displayName',
         render: (val: any, row: any) => {
           return (
             <Button type="text" onClick={() => handleDelRow(val)}>
@@ -46,11 +45,11 @@ const FieldTable = forwardRef(
       let _data = [...tbData];
       if (typeof fid === 'string') {
         _data = _data.filter((item) => {
-          return item.fieldUuid !== fid;
+          return item.displayName !== fid;
         });
       } else if (Array.isArray(fid)) {
         _data = _data.filter((item) => {
-          return fid.indexOf(item.fieldUuid) < 0;
+          return fid.indexOf(item.displayName) < 0;
         });
       }
       setTableData(_data);
@@ -67,7 +66,7 @@ const FieldTable = forwardRef(
       if (Array.isArray(tbData)) {
         let cur_key_arr: any[] = [];
         tbData.forEach((item: any) => {
-          cur_key_arr.push(item.fieldUuid);
+          cur_key_arr.push(item.displayName);
         });
         setCurKeyArr(cur_key_arr);
       }
@@ -92,7 +91,7 @@ const FieldTable = forwardRef(
         </div>
         <Table
           className="field-table-wrapper"
-          rowKey="fieldUuid"
+          rowKey="displayName"
           columns={[...(columnsTable || []), ...baseColumns]}
           data={tbData}
           pagination={false}
@@ -117,9 +116,3 @@ const FieldTable = forwardRef(
   }
 );
 export default FieldTable;
-
-
-
-
-
-
