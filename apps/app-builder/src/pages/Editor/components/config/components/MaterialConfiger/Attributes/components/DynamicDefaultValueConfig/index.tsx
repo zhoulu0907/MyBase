5ad882f21e71661e05a72bba2cf62cf53e55bfcd
ext_chenyongqiang <chenyongqiang@richinfo.cn>
2025-12-10 14:06:13 +0800

    S25029301301-4 组件默认值根据类型修改

--- apps/app-builder/src/pages/Editor/components/config/components/MaterialConfiger/Attributes/components/DynamicDefaultValueConfig/index.tsx
+++ apps/app-builder/src/pages/Editor/components/config/components/MaterialConfiger/Attributes/components/DynamicDefaultValueConfig/index.tsx
@@ -1,8 +1,14 @@
-import { Form, Select, Input, Button, Switch, DatePicker, TimePicker } from '@arco-design/web-react';
+import { Form, Select, Input, Button, Switch, DatePicker, TimePicker, InputNumber } from '@arco-design/web-react';
 import { useEffect, useState } from 'react';
 import { IconLaunch } from '@arco-design/web-react/icon';
 import { registerConfigRenderer } from '../../registry';
-import { CONFIG_TYPES, DEFAULT_VALUE_TYPES, DEFAULT_VALUE_TYPES_LABELS, getPopupContainer } from '@onebase/ui-kit';
+import {
+  CONFIG_TYPES,
+  DEFAULT_VALUE_TYPES,
+  DEFAULT_VALUE_TYPES_LABELS,
+  PHONE_TYPE,
+  getPopupContainer
+} from '@onebase/ui-kit';
 import { FormulaEditor } from '@/components/FormulaEditor';
 import styles from '../../index.module.less';
 
@@ -26,6 +32,7 @@ const DynamicDefaultValueConfig: React.FC<DynamicDefaultValueConfigProps> = ({
     customValue: undefined,
     formulaValue: undefined
   });
+  const [errorText, setErrorText] = useState('');
   // 公式计算弹窗
   const [formulaVisible, setFormulaVisible] = useState<boolean>(false);
 
@@ -59,7 +66,11 @@ const DynamicDefaultValueConfig: React.FC<DynamicDefaultValueConfigProps> = ({
       <Form.Item layout="vertical" label={item.name || '默认值'} className={styles.formItem}>
         <Select
           getPopupContainer={getPopupContainer}
-          onChange={(value) => handleChange('type', value)}
+          onChange={(value) => {
+            setErrorText(``);
+            const newConfig = { ...configs[defaultValueConfigKey], type: value, formulaValue: '', customValue: '' };
+            handlePropsChange(defaultValueConfigKey, newConfig);
+          }}
           value={defaultValueConfig?.type}
           options={[
             { label: DEFAULT_VALUE_TYPES_LABELS[DEFAULT_VALUE_TYPES.CUSTOM], value: DEFAULT_VALUE_TYPES.CUSTOM },
@@ -72,10 +83,6 @@ const DynamicDefaultValueConfig: React.FC<DynamicDefaultValueConfigProps> = ({
       {defaultValueConfig?.type === DEFAULT_VALUE_TYPES.CUSTOM && (
         <Form.Item layout="vertical" className={styles.formItem}>
           {item.valueType === 'boolean' && (
-            // <Switch
-            //   checked={defaultValueConfig?.customValue}
-            //   onChange={(value) => handleChange('customValue', value)}
-            // />
             <Select
               options={[
                 { label: '开启', value: 'true' },
@@ -120,6 +127,49 @@ const DynamicDefaultValueConfig: React.FC<DynamicDefaultValueConfigProps> = ({
               onChange={(value) => handleChange('customTimeValue', value)}
             ></TimePicker>
           )}
+
+          {item.valueType === 'phone' && (
+            <Input
+              value={defaultValueConfig?.customValue}
+              maxLength={configs.phoneType === PHONE_TYPE.MOBILE ? 11 : 15}
+              status={errorText ? 'error' : undefined}
+              onChange={(value) => {
+                if (configs.phoneType === PHONE_TYPE.MOBILE) {
+                  if (value && !/^1[3-9]\d{9}$/.test(value)) {
+                    setErrorText(`请输入有效的11位中国大陆手机号`);
+                  } else {
+                    setErrorText(``);
+                  }
+                }
+                if (configs.phoneType === PHONE_TYPE.LANDLINE) {
+                  if (value && !/^\(?0[0-9]{2,3}\)?-?[0-9]{7,8}$/.test(value)) {
+                    setErrorText(`请输入有效的座机号`);
+                  } else {
+                    setErrorText(``);
+                  }
+                }
+                handleChange('customValue', value);
+              }}
+              placeholder="请输入"
+            />
+          )}
+          {item.valueType === 'number' && (
+            <InputNumber
+              step={configs.step}
+              min={configs.verify?.numberLimit ? configs.verify?.min : undefined}
+              max={configs.verify?.numberLimit ? configs.verify?.max : undefined}
+              precision={configs?.numberFormat.showPrecision ? configs.numberFormat.precision : 0}
+              formatter={(value) => {
+                return configs?.numberFormat.useThousandsSeparator
+                  ? `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  : value.toString();
+              }}
+              parser={(value) => value.replace(/,/g, '')}
+              suffix={configs?.numberFormat.showUnit ? configs.numberFormat.unitValue : ''}
+            />
+          )}
+
+          {errorText && <div style={{ color: '#f53f3f', fontSize: '12px' }}>{errorText}</div>}
         </Form.Item>
       )}
       {/* 公式计算 */}
