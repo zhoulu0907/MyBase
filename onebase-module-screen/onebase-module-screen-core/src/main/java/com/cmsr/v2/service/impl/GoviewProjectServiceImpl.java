a898b3f0c64601c976a49df2ba19f6940432c90a
guanshipeng <2533280561@qq.com>
2025-12-11 10:36:03 +0800

    feat(project): 添加GoviewProjectServiceImpl服务实现类
    - 创建GoviewProjectServiceImpl类继承ServiceImpl- 实现IGoviewProjectService接口- 注入GoviewProjectMapper数据访问层
    - 添加@Service注解标记为Spring服务组件
    - 配置基础的CRUD操作支持
    - 设置文件头部注释和作者信息

---
+++ onebase-module-screen/onebase-module-screen-core/src/main/java/com/cmsr/v2/service/impl/GoviewProjectServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cmsr.v2.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cmsr.v2.mapper.GoviewProjectMapper;
+import com.cmsr.v2.model.GoviewProject;
+import com.cmsr.v2.service.IGoviewProjectService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author fc
+ * @since 2023-04-30
+ */
+@Service
+public class GoviewProjectServiceImpl extends ServiceImpl<GoviewProjectMapper, GoviewProject> implements IGoviewProjectService {
+
+}
