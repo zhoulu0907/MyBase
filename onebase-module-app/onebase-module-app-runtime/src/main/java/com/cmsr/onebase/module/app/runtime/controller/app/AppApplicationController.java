6b7dcd5eac85163a09281b3f0904616cf53f2f5f
matianyu <litesuits@qq.com>
2025-12-17 11:06:55 +0800

    feat(system): app get 权限

--- onebase-module-app/onebase-module-app-runtime/src/main/java/com/cmsr/onebase/module/app/runtime/controller/app/AppApplicationController.java
+++ onebase-module-app/onebase-module-app-runtime/src/main/java/com/cmsr/onebase/module/app/runtime/controller/app/AppApplicationController.java
@@ -1,12 +1,14 @@
 package com.cmsr.onebase.module.app.runtime.controller.app;
 
 import com.cmsr.onebase.framework.common.pojo.CommonResult;
+import com.cmsr.onebase.framework.tenant.core.aop.TenantIgnore;
 import com.cmsr.onebase.module.app.core.vo.app.ApplicationNavigationConfigVO;
 import com.cmsr.onebase.module.app.runtime.service.app.AppApplicationService;
 import com.cmsr.onebase.module.app.runtime.vo.app.ApplicationRespVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.annotation.security.PermitAll;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
