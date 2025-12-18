f9b4a7c254d797956c20eb9077c22bcddc8e87d2
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-12-18 14:13:05 +0800

    S25029301301-3218 dev

--- apps/app-builder/src/main.tsx
+++ apps/app-builder/src/main.tsx
@@ -3,8 +3,10 @@ import '@arco-themes/react-cyansu-ob03/index.less';
 import '@onebase/ui-kit/src/theme.less';
 
 import { ConfigProvider } from '@arco-design/web-react';
+import { ErrorPage } from '@onebase/common';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.tsx';
 import './i18n';
 import './index.css';
@@ -12,9 +14,9 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfigProvider prefixCls="pc">
-      {/* <ErrorBoundary FallbackComponent={ErrorPage}> */}
-      <App />
-      {/* </ErrorBoundary> */}
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+        <App />
+      </ErrorBoundary>
     </ConfigProvider>
   </StrictMode>
 );
