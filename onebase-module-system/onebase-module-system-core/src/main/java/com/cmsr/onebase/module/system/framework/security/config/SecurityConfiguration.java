07652f8f194067ba4cefb2f19ac9d01df2288dc7
matianyu <litesuits@qq.com>
2025-12-19 17:45:43 +0800

    refactor(config): token 加解密

--- onebase-module-system/onebase-module-system-core/src/main/java/com/cmsr/onebase/module/system/framework/security/config/SecurityConfiguration.java
+++ onebase-module-system/onebase-module-system-core/src/main/java/com/cmsr/onebase/module/system/framework/security/config/SecurityConfiguration.java
@@ -20,7 +20,8 @@ public class SecurityConfiguration {
     public SM2 sm2() {
         // 使用指定私钥初始化 SM2
         String privateKey = ENConstant.EN_P_SM2KEY;
-        SM2 sm2 = new SM2(privateKey, null);
+        String publicKey = ENConstant.EN_P_SM2KEY_PUBLIC;
+        SM2 sm2 = new SM2(privateKey, publicKey);
         // 使用 C1C3C2 模式解密,与前端保持一致
         sm2.setMode(SM2Engine.Mode.C1C3C2);
         return sm2;
