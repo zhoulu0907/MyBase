195315d26690129a4052eb10da87f89a34db5062
jinyonghu <jinyonghu@cmsr.chinamobile.com>
2025-12-22 10:26:29 +0800

    S25029301301-752 h5登录问题修复

--- apps/mobile-runtime/src/pages/Login/components/right.tsx
+++ apps/mobile-runtime/src/pages/Login/components/right.tsx
@@ -59,8 +59,13 @@ const Right: React.FC = () => {
     console.log('rawHash.startsWith(prefix)', rawHash.startsWith(prefix));
     if (rawHash.startsWith(prefix)) {
       const redirectURL = rawHash.replace(prefix, '');
-      const aid = getHashQueryParam('appId', redirectURL) || '';
-      const tid = getHashQueryParam('tenantId', redirectURL) || '';
+      let aid = getHashQueryParam('appId', redirectURL) || '';
+      let tid = getHashQueryParam('tenantId', redirectURL) || '';
+      if (!appId) {
+        const pathRedirect = (redirectURL.split('#/')[1] || '').split('/');
+        aid = pathRedirect[1] || '';
+        tid = pathRedirect[2] || tid || '';
+      }
       setAppId(aid);
       setTenantId(tid);
     } else {
