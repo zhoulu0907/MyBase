859fc439d1220845e1dc9f8dad28bc766ad29981
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2025-12-23 12:30:20 +0800

    S25029301301-3218 fix bug

--- packages/common/src/components/Upload/upload.tsx
+++ packages/common/src/components/Upload/upload.tsx
@@ -3,16 +3,16 @@ import { Cropper } from '../Cropper';
 import { UploadSizeConfig } from './uploadAvatar';
 
 interface IUploadProps {
-    uploadRef: any;
-    size?:UploadSizeConfig;
-    aspect?: number;
-    onUpdateUrl: (value: string) => void;
-    getUploadFile: Function;
-    imagePreview?: boolean;
+  uploadRef: any;
+  size?: UploadSizeConfig;
+  aspect?: number;
+  onUpdateUrl: (value: string) => void;
+  getUploadFile: Function;
+  imagePreview?: boolean;
 }
 
-const UploadCommonComponent:React.FC<IUploadProps> = ({size, aspect, uploadRef, getUploadFile,onUpdateUrl}) => {
-    const handleUpload = async (file: File, onProgress?: (percent: number, event?: ProgressEvent) => void) => {
+const UploadCommonComponent: React.FC<IUploadProps> = ({ size, aspect, uploadRef, getUploadFile, onUpdateUrl }) => {
+  const handleUpload = async (file: File, onProgress?: (percent: number, event?: ProgressEvent) => void) => {
     const formData = new FormData();
     formData.append('file', file);
 
@@ -29,67 +29,68 @@ const UploadCommonComponent:React.FC<IUploadProps> = ({size, aspect, uploadRef, 
     return res;
   };
 
-    return  (
-        <Upload
-            ref={uploadRef}
-            limit={1}
-            accept="image/*"
-            listType="picture-card"
-            showUploadList={false}
-            customRequest={async (option) => {
-                const { onProgress, onError, onSuccess, file } = option;
-                try {
-                const uploadUrl = await handleUpload(file, onProgress);
-                if (uploadUrl !== '') {
-                    onUpdateUrl(uploadUrl);
-                    onSuccess(uploadUrl);
-                } else {
-                    onError({
-                    status: 'error',
-                    msg: '上传失败'
-                    });
-                }
-                } catch (error) {
-                onError({
-                    status: 'error',
-                    msg: '上传失败'
-                });
-                }
-            }}
-            beforeUpload={(file) => {
-                return new Promise((resolve) => {
-                const modal = Modal.confirm({
-                    title: '裁剪图片',
-                    onCancel: () => {
-                    Message.info('取消上传');
-                    resolve(false);
-                    modal.close();
-                    },
-                    simple: false,
-                    content: (
-                    <Cropper
-                        aspect={aspect ? aspect : size?.aspect}
-                        file={file}
-                        onOK={(file: any) => {
-                        resolve(file);
-                        modal.close();
-                        }}
-                        onCancel={() => {
-                        resolve(false);
-                        Message.info('取消上传');
-                        modal.close();
-                        }}
-                    />
-                    ),
-                    footer: null
-                });
-                });
-            }}
-            style={{
-                display: 'none'
-            }}
-        ></Upload>
-    )
-}
+  return (
+    <Upload
+      ref={uploadRef}
+      limit={1}
+      accept="image/*"
+      listType="picture-card"
+      showUploadList={false}
+      customRequest={async (option) => {
+        const { onProgress, onError, onSuccess, file } = option;
+        try {
+          const resourceId = await handleUpload(file, onProgress);
+
+          if (resourceId !== '') {
+            onUpdateUrl(resourceId);
+            onSuccess(resourceId);
+          } else {
+            onError({
+              status: 'error',
+              msg: '上传失败'
+            });
+          }
+        } catch (error) {
+          onError({
+            status: 'error',
+            msg: '上传失败'
+          });
+        }
+      }}
+      beforeUpload={(file) => {
+        return new Promise((resolve) => {
+          const modal = Modal.confirm({
+            title: '裁剪图片',
+            onCancel: () => {
+              Message.info('取消上传');
+              resolve(false);
+              modal.close();
+            },
+            simple: false,
+            content: (
+              <Cropper
+                aspect={aspect ? aspect : size?.aspect}
+                file={file}
+                onOK={(file: any) => {
+                  resolve(file);
+                  modal.close();
+                }}
+                onCancel={() => {
+                  resolve(false);
+                  Message.info('取消上传');
+                  modal.close();
+                }}
+              />
+            ),
+            footer: null
+          });
+        });
+      }}
+      style={{
+        display: 'none'
+      }}
+    ></Upload>
+  );
+};
 
 export { UploadCommonComponent };
