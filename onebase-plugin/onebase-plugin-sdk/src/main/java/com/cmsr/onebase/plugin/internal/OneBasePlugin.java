059df84eb2cfdef1921aa745995b878d98f70dea
chengyuansen <chengyuansen@cmsr.chinamobile.com>
2025-12-24 17:30:35 +0800

    refactor(plugin): 移除日志依赖并优化插件基类设计
    - 移除了 OneBasePlugin 中的 slf4j 日志依赖和 Logger 实例
    - 更新了插件生命周期方法的文档注释格式，移除多余的缩进
    - 在 start、stop、delete 方法中移除日志输出，改为提供可覆盖的模板方法
    - 从 onebase-plugin-sdk 的 pom.xml 中移除 slf4j-api 和其他可选依赖
    - 在 onebase-spring-boot-starter-plugin 中排除 reload4j 依赖以避免日志冲突
    - 优化了插件基类的扩展性，为子类提供更清晰的自定义接口

--- onebase-plugin/onebase-plugin-sdk/src/main/java/com/cmsr/onebase/plugin/internal/OneBasePlugin.java
+++ onebase-plugin/onebase-plugin-sdk/src/main/java/com/cmsr/onebase/plugin/internal/OneBasePlugin.java
@@ -2,8 +2,6 @@ package com.cmsr.onebase.plugin.internal;
 
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * OneBase插件基类
@@ -14,10 +12,10 @@ import org.slf4j.LoggerFactory;
  * <p>
  * 生命周期：
  * <ol>
- *     <li>构造函数 - 插件被加载时调用</li>
- *     <li>start() - 插件启动时调用</li>
- *     <li>stop() - 插件停止时调用</li>
- *     <li>delete() - 插件被删除时调用</li>
+ * <li>构造函数 - 插件被加载时调用</li>
+ * <li>start() - 插件启动时调用</li>
+ * <li>stop() - 插件停止时调用</li>
+ * <li>delete() - 插件被删除时调用</li>
  * </ol>
  * </p>
  *
@@ -25,8 +23,6 @@ import org.slf4j.LoggerFactory;
  * @date 2025-12-18
  */
 public class OneBasePlugin extends Plugin {
-
-    private static final Logger log = LoggerFactory.getLogger(OneBasePlugin.class);
 
     /**
      * PF4J要求的构造函数
@@ -50,32 +46,35 @@ public class OneBasePlugin extends Plugin {
      * 插件启动
      * <p>
      * 插件被启动时调用，可以在此进行初始化操作。
+     * 子类可以覆盖此方法进行自定义初始化。
      * </p>
      */
     @Override
     public void start() {
-        log.info("插件 [{}] 启动", getPluginId());
+        // 插件启动 - 子类可以覆盖此方法进行初始化
     }
 
     /**
      * 插件停止
      * <p>
      * 插件被停止时调用，可以在此进行资源清理操作。
+     * 子类可以覆盖此方法进行自定义清理。
      * </p>
      */
     @Override
     public void stop() {
-        log.info("插件 [{}] 停止", getPluginId());
+        // 插件停止 - 子类可以覆盖此方法进行清理
     }
 
     /**
      * 插件删除
      * <p>
      * 插件被卸载删除时调用。
+     * 子类可以覆盖此方法进行自定义清理。
      * </p>
      */
     @Override
     public void delete() {
-        log.info("插件 [{}] 删除", getPluginId());
+        // 插件删除 - 子类可以覆盖此方法
     }
 }
