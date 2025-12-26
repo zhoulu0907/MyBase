76c93799cade8c21a1b2eaa46ba276496328f17e
ext_chenyongqiang <chenyongqiang@richinfo.cn>
2025-12-26 10:17:43 +0800

    S25029301301-4 移动端外部用户登录

--- apps/mobile-runtime/src/pages/ThirdLogin/components/ConfirmInfoForm/index.tsx
+++ apps/mobile-runtime/src/pages/ThirdLogin/components/ConfirmInfoForm/index.tsx
@@ -1,4 +1,6 @@
 import { Tabs, Form, Input, Button, Toast, Popup } from '@arco-design/mobile-react';
+import { IconArrowBack } from '@arco-design/mobile-react/esm/icon';
+
 import styles from '../../index.module.less';
 
 interface IConfirmInfoProps {
@@ -11,10 +13,14 @@ interface IConfirmInfoProps {
 
 const ConfirmInfoForm: React.FC<IConfirmInfoProps> = ({ visible, appId, tenantId, onOk, onCancel }) => {
   return (
-    <Popup visible={visible} close={onCancel} direction="bottom" maskClosable={false}>
-      <div style={{ height: 330, width: 290 }}>
-        <div className="popup-demo-title">Title</div>
-        <div className="popup-demo-content">Content area</div>
+    <Popup visible={visible} close={onCancel} direction="bottom" maskClosable={false} className={styles.popup}>
+      <div className={styles.popupContent}>
+        <div className={styles.popupBack}>
+          <IconArrowBack />
+        </div>
+        <div className={styles.popupMain}>
+          <IconArrowBack />
+        </div>
       </div>
     </Popup>
   );
