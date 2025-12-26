69f3659c3f16b92a4d149cb46b0bf3dbacc2bac7
ext_lish <lishuhao004@chinasoftinc.com>
2025-12-26 17:59:34 +0800

    SZHDZ-3331 修改创建大屏时获取模版列表接口

--- packages/app/src/services/app_dashboard.ts
+++ packages/app/src/services/app_dashboard.ts
@@ -3,7 +3,7 @@ import { DashboardListParams, DashboardTemplateListParams, editDashboardInfoPara
 
 // 获取模版列表接口
 export const getDashboardTemplateListApi = (params: DashboardTemplateListParams) => {
-  return dashboardService.get(`/template/page?hot=${params.hot}&templateType=${params.templateType}&pageNo=${params.pageNo}&pageSize=${params.pageSize}&templateName=${params.templateName}`); 
+  return dashboardService.get(`/template/page`, params); 
 };
 
 // 获取大屏列表接口 
