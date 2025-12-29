e22f6b2b3674caa7f9bb2b839210605e95b59ae7
yangxiaoyi <yangxiaoyi@cmsr.chinamobile.com>
2025-12-29 16:29:45 +0800

    S25029301301-1483 工作台-增加欢迎卡片组件

--- packages/ui-kit/src/components/render/EditRender/index.tsx
+++ packages/ui-kit/src/components/render/EditRender/index.tsx
@@ -143,6 +143,8 @@ const ComponentEditRender: React.FC<ComponentRenderProps> = ({ cpId, cpType, pag
         return <WorkbenchComp.XRichTextEditorWorkbench cpName={cpId} id={cpId} {...componentConfig} runtime={runtime} />;
       case WORKBENCH_COMPONENT_TYPES.CAROUSEL_WORKBENCH:
         return <WorkbenchComp.XCarouselWorkbench cpName={cpId} id={cpId} {...componentConfig} runtime={runtime} />;
+      case WORKBENCH_COMPONENT_TYPES.WELCOME_CARD:
+        return <WorkbenchComp.XWelcomeCard cpName={cpId} id={cpId} {...componentConfig} runtime={runtime} />;
       case WORKBENCH_COMPONENT_TYPES.BUTTON_WORKBENCH:
         // 移动端独有组件，web 端静默处理
         return null;
