c7c49dd49626f20fbe93f922726196a62ff58139
yuxin <yu.x-neu@neusoft.com>
2025-12-29 09:25:45 +0800

    refactor(dashboard): 重构从页面创建数据大屏服务实现

--- onebase-module-app/onebase-module-app-core/src/main/java/com/cmsr/onebase/module/app/core/provider/resource/DashboardServiceProvider.java
+++ onebase-module-app/onebase-module-app-core/src/main/java/com/cmsr/onebase/module/app/core/provider/resource/DashboardServiceProvider.java
@@ -3,8 +3,8 @@ package com.cmsr.onebase.module.app.core.provider.resource;
 import com.cmsr.onebase.framework.common.exception.util.ServiceExceptionUtil;
 import com.cmsr.onebase.framework.common.security.ApplicationManager;
 import com.cmsr.onebase.module.app.core.enums.resource.AppResourceErrorCodeConstants;
-import com.cmsr.onebase.module.screen.api.DashboardApi;
-import com.cmsr.onebase.module.screen.api.dto.GoViewProjectDTO;
+import com.cmsr.onebase.module.screen.api.DashboardProjectApi;
+import com.cmsr.onebase.module.screen.api.dto.DashboardProjectDTO;
 import com.cmsr.onebase.module.screen.api.enums.DashboardCreateTypeSetEnum;
 import jakarta.annotation.Resource;
 import lombok.Setter;
@@ -18,13 +18,13 @@ import java.util.Objects;
 public class DashboardServiceProvider {
 
     @Resource
-    private DashboardApi dashboardApi;
+    private DashboardProjectApi dashboardApi;
 
     public Long createDashboard(String createType, Long dashboardId) {
         //根据数据大屏的创建类型创建数据大屏页面
         if (Objects.equals(createType, DashboardCreateTypeSetEnum.DASHBOARD_LINK.getCode())){
             //1.1 如果是绑定现有大屏，则查询数据大屏信息，如不存在则报错
-            List<GoViewProjectDTO> dashboardList = dashboardApi.getDashboard(dashboardId);
+            List<DashboardProjectDTO> dashboardList = dashboardApi.getDashboard(dashboardId);
             if (dashboardList.isEmpty()){
                 throw ServiceExceptionUtil.exception(AppResourceErrorCodeConstants.DASHBOARD_NOT_EXIST);
             }
@@ -37,7 +37,7 @@ public class DashboardServiceProvider {
             return newDashboardId;
         } else {
             //1.3 从空白页创建数据大屏
-            GoViewProjectDTO goViewProjectDTO = new GoViewProjectDTO();
+            DashboardProjectDTO goViewProjectDTO = new DashboardProjectDTO();
 
             goViewProjectDTO.setProjectName("新大屏");
             goViewProjectDTO.setState(-1);
@@ -48,7 +48,7 @@ public class DashboardServiceProvider {
         return null;
     }
 
-    public List<GoViewProjectDTO> getDashboard(Long dashboardId){
+    public List<DashboardProjectDTO> getDashboard(Long dashboardId){
         return dashboardApi.getDashboard(dashboardId);
     }
 }
