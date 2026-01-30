cf6aa5762889d49776056efeeb8ff78b537c7892
ext_chenyongqiang <chenyongqiang@richinfo.cn>
2026-01-30 16:18:14 +0800

    S25029301301-3179 图片、文件组件校验判断修改

--- packages/ui-kit/src/components/Materials/Basic/FormComponents/FileUpload/index.tsx
+++ packages/ui-kit/src/components/Materials/Basic/FormComponents/FileUpload/index.tsx
@@ -207,7 +207,7 @@ const XFileUpload = memo(
               if (verify?.fileFormatLimit && verify?.fileFormat) {
                 const lastIndexOf = file.name.lastIndexOf('.');
                 const type = file.name.slice(lastIndexOf + 1);
-                if (verify.fileFormat.toLocaleLowerCase().indexOf(type.toLocaleLowerCase()) === -1) {
+                if (verify.fileFormat.toLocaleLowerCase().split(',').includes(type.toLocaleLowerCase())) {
                   Message.warning(`不支持该格式，仅支持 ${verify.fileFormat}`);
                   return false;
                 }
