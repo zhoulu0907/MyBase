4ac181e3e9480da0b44b9aa85df5fcf702e126f7
zhoumingji <zhoumingji@cmsr.chinamobile.com>
2026-02-05 08:40:39 +0800

    S25029301301-2724 themes dev

--- apps/mobile-runtime/src/main.tsx
+++ apps/mobile-runtime/src/main.tsx
@@ -1,7 +1,7 @@
 // 导入Arco Design Mobile React的样式
 import '@arco-design/mobile-react/dist/style.css';
 // import '@arco-themes/react-cyansu-ob03/index.less';
-// import '@onebase/ui-kit/src/theme.less';
+import '@onebase/ui-kit/src/theme.less';
 
 import setRootPixel from '@arco-design/mobile-react/tools/flexible';
 import { ErrorPage } from '@onebase/common';
