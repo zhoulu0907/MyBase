8f3a2bf5309cb6b7977042bb18236318213e6637
cube-devin <yutian.shi@outlook.com>
2026-02-06 10:58:06 +0800

    dev

--- onebase-plugin/onebase-plugin-build/src/main/java/com/cmsr/onebase/plugin/build/config/PluginSecurityConfiguration.java
+++ onebase-plugin/onebase-plugin-build/src/main/java/com/cmsr/onebase/plugin/build/config/PluginSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.cmsr.onebase.plugin.build.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,12 +22,12 @@ public class PluginSecurityConfiguration {
     /**
      * 配置静态资源的安全过滤器链
      * <p>
-     * 显式配置过滤器链并允许匿名访问，优先级设置为 0 (最高优先级之一)，
+     * 显式配置过滤器链并允许匿名访问，优先级设置为最高，
      * 确保在其他通用过滤器链之前生效。
      * </p>
      */
     @Bean
-    @Order(0)
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain pluginStaticResourcesFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/plugins/static/**")
