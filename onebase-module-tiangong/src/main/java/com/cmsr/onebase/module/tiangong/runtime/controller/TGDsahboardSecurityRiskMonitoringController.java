a4c7bce1fa320eabb55a4b8f5b9ff2269efdc340
ext_guanshipeng <2533280561@qq.com>
2026-02-27 16:55:27 +0800

    feat(tiangong): 更新摄像机截屏响应VO和数据内容
    - 修改CameraScreenshotRespVO字段名称为cameraInfo、address和timeInfo
    - 替换原有的column1、column2、column3字段
    - 更新控制器中的测试数据内容为实际的摄像机信息
    - 修改createCameraScreenshot方法参数名称以匹配新的字段结构
    - 调整方法内部属性赋值逻辑以使用新字段名

--- onebase-module-tiangong/src/main/java/com/cmsr/onebase/module/tiangong/runtime/controller/TGDsahboardSecurityRiskMonitoringController.java
+++ onebase-module-tiangong/src/main/java/com/cmsr/onebase/module/tiangong/runtime/controller/TGDsahboardSecurityRiskMonitoringController.java
@@ -121,16 +121,16 @@ public class TGDsahboardSecurityRiskMonitoringController {
     @Operation(summary = "获取当前摄像机截屏列表")
     public CommonResult<List<CameraScreenshotRespVO>> getCameraScreenshotList() {
         List<CameraScreenshotRespVO> screenshotList = List.of(
-                createCameraScreenshot(1, "行1列1", "行1列2", "行1列3"),
-                createCameraScreenshot(2, "行2列1", "行2列2", "行2列3"),
-                createCameraScreenshot(3, "行3列1", "行3列2", "行3列3"),
-                createCameraScreenshot(4, "行4列1", "行4列2", "行4列3"),
-                createCameraScreenshot(5, "行5列1", "行5列2", "行5列3"),
-                createCameraScreenshot(6, "行6列1", "行6列2", "行6列3"),
-                createCameraScreenshot(7, "行7列1", "行7列2", "行7列3"),
-                createCameraScreenshot(8, "行8列1", "行8列2", "行8列3"),
-                createCameraScreenshot(9, "行9列1", "行9列2", "行9列3"),
-                createCameraScreenshot(10, "行10列1", "行10列2", "行10列3")
+                createCameraScreenshot(1, "人员12", "A区 一楼", "2分钟前"),
+                createCameraScreenshot(2, "人员12", "A区 一楼", "3分钟前"),
+                createCameraScreenshot(3, "人员12", "A区 一楼", "4分钟前"),
+                createCameraScreenshot(4, "人员12", "A区 一楼", "5分钟前"),
+                createCameraScreenshot(5, "人员12", "A区 一楼", "6分钟前"),
+                createCameraScreenshot(6, "人员12", "A区 一楼", "7分钟前"),
+                createCameraScreenshot(7, "人员12", "A区 一楼", "8分钟前"),
+                createCameraScreenshot(8, "人员12", "A区 一楼", "9分钟前"),
+                createCameraScreenshot(9, "人员12", "A区 一楼", "10分钟前"),
+                createCameraScreenshot(10, "人员12", "A区 一楼", "11分钟前")
         );
         return success(screenshotList);
     }
@@ -158,12 +158,12 @@ public class TGDsahboardSecurityRiskMonitoringController {
         return vo;
     }
 
-    private CameraScreenshotRespVO createCameraScreenshot(Integer serialNumber, String column1, String column2, String column3) {
+    private CameraScreenshotRespVO createCameraScreenshot(Integer serialNumber, String cameraInfo, String address, String timeInfo) {
         CameraScreenshotRespVO vo = new CameraScreenshotRespVO();
         vo.setSerialNumber(serialNumber);
-        vo.setColumn1(column1);
-        vo.setColumn2(column2);
-        vo.setColumn3(column3);
+        vo.setCameraInfo(cameraInfo);
+        vo.setAddress(address);
+        vo.setTimeInfo(timeInfo);
         return vo;
     }
 }
