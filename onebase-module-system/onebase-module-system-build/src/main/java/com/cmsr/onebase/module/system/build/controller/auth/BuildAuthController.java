aff74137f248b61dd8a82e6739ba9758383b9e12
yuxin <yu.x-neu@neusoft.com>
2026-03-05 11:19:41 +0800

    chore(build): 移除未使用的导入

--- onebase-module-system/onebase-module-system-build/src/main/java/com/cmsr/onebase/module/system/build/controller/auth/BuildAuthController.java
+++ onebase-module-system/onebase-module-system-build/src/main/java/com/cmsr/onebase/module/system/build/controller/auth/BuildAuthController.java
@@ -27,8 +27,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 import static com.cmsr.onebase.framework.common.pojo.CommonResult.success;
 
 /**
