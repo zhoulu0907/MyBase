aab5fb63b0de89cb570bddea3418bca17d77c304
cube-devin <yutian.shi@outlook.com>
2026-03-20 15:47:23 +0800

    修改相关数据标签

--- apps/app-builder/src/pages/Setting/pages/SpaceInfo/Tags.tsx
+++ apps/app-builder/src/pages/Setting/pages/SpaceInfo/Tags.tsx
@@ -46,15 +46,15 @@ const Tags = ({ data }: IProps) => {
             content={
               <div style={{ width: 264 }}>
                 <div style={{ display: 'flex', marginBottom: 16 }}>
-                  <span style={{ width: 70, color: 'var(--color-text-3)' }}>手机号</span>
+                  <span style={{ width: 80, color: 'var(--color-text-3)' }}>手机号</span>
                   <span>{tag.adminMobile || '-'}</span>
                 </div>
                 <div style={{ display: 'flex', marginBottom: 16 }}>
-                  <span style={{ width: 70, color: 'var(--color-text-3)' }}>邮箱</span>
+                  <span style={{ width: 80, color: 'var(--color-text-3)' }}>邮箱</span>
                   <span>{tag.adminEmail || '-'}</span>
                 </div>
                 <div style={{ display: 'flex' }}>
-                  <span style={{ width: 70, color: 'var(--color-text-3)' }}>所属部门</span>
+                  <span style={{ width: 80, color: 'var(--color-text-3)' }}>所属部门</span>
                   <span>{tag.deptName || '-'}</span>
                 </div>
               </div>
