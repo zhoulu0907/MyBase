34a69d81423fb4d09b36c9ae55b2123261992edd
zhangliang <zhangliang@cmsr.chinamobile.com>
2026-04-03 11:02:37 +0800

    APAAS-2 优化配置加载逻辑

--- packages/common/src/utils/env.ts
+++ packages/common/src/utils/env.ts
@@ -21,7 +21,9 @@ export const getProdConfig = (key = 'CONFIG') => {
   return null;
 };
 
-export const envConfig = process.env.NODE_ENV === 'production' ? getProdConfig() : ((window as any).global_config || DEFAULT_CONFIG);
+export const envConfig = process.env.NODE_ENV === 'production' 
+  ? getProdConfig(window.location.hostname.includes('artifex-cmcc') ? 'ARTIFEX_CONFIG' : 'CONFIG')
+  : ((window as any).global_config || DEFAULT_CONFIG);
 export const aiConfig = process.env.NODE_ENV === 'production' ? getProdConfig('AI_CONFIG') : ((window as any).global_config || DEFAULT_CONFIG);
 console.log("engconfig:" , envConfig)
 // 平台、空间、应用端环境
