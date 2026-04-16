c6aebd477a3bb6758fb798d22be2b21d4ea7df3a
leon546 <54623430@qq.com>
2026-04-16 20:41:21 +0800

    APAAS-2 样式修改

--- apps/app-builder/src/components/TenantLogo/index.tsx
+++ apps/app-builder/src/components/TenantLogo/index.tsx
@@ -2,6 +2,9 @@ import { Image } from '@arco-design/web-react';
 import styles from './index.module.less';
 import { getFileUrlById, type TenantInfo } from '@onebase/platform-center';
 import LogoSVG from '@/assets/images/app_header_logo.svg';
+import TiangongLogoSVG from '@/assets/images/tiangong_app_header_logo.svg';
+
+const isArtifex = typeof window !== 'undefined' && window.location.hostname.includes('artifex');
 
 interface ITenantLogoProps {
   tenantInfo: TenantInfo | null;
@@ -10,7 +13,7 @@ interface ITenantLogoProps {
 const TenantLogo: React.FC<ITenantLogoProps> = ({ tenantInfo }) => {
   return (
     <>
-      <Image src={LogoSVG} alt="logo" height={28} />
+      <Image src={isArtifex ? TiangongLogoSVG : LogoSVG} alt="logo" height={28} />
       <div className={styles.line}></div>
       {(tenantInfo?.logoUrl && <Image src={getFileUrlById(tenantInfo.logoUrl)} height={28} alt="tenant-logo" />)}
       <div className={styles.tenantName}>{tenantInfo?.name}</div>
